6c014ded5f1f8463974960a2c9d1675d4955e018
Ron Fox <fox@nscl.msu.edu>
2001-08-17 13:19:02 +0000

    This commit was manufactured by cvs2svn to create branch 'NSCL'.

--- SpecTcl/SpecTcl/doc/tailoring/examples/_vti_cnf/tclpackagedcommand.h
+++
@@ -1,8 +0,0 @@
-vti_encoding:SR|utf8-nl
-vti_author:SR|FOX
-vti_timecreated:TR|25 Jun 1999 11:34:44 -0400
-vti_modifiedby:SR|FOX
-vti_backlinkinfo:VX|
-vti_extenderversion:SR|3.0.2.1330
-vti_timelastmodified:TR|25 Jun 1999 11:34:44 -0400
-vti_syncwith_localhost\\j\:\\public_html\\spectcl/j\:/public_html/spectcl:TR|25 Jun 1999 11:34:44 -0400
