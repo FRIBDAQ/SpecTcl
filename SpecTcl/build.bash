21175d557a087f7f13e76f7e6a1f59419fddbb1a
Ron Fox <fox@nscl.msu.du>
2001-09-12 00:53:17 +0000

    Debug build.bash for Cygwin, it'll also work in other bash builds. May want to phase out build.sh, or push up the use of autoconf.

--- SpecTcl/build.bash
+++ SpecTcl/build.bash
@@ -3,32 +3,31 @@
 #  Needed for Cygwin builds where csh doesn't exist.
 #
 
-
+export PATH=$PATH:/usr/X11R6/bin    # makedepend can be here.
 export OS=`uname | sed s/-//`       # the sed is needed for HP*-*UX
 
 # CYGWin will now look like: CYGWIN_winveresion.
 # We want to get CYGWIN out of it. 
 case $OS in 
   CYGWIN*)
-    export OS=CYGWIN
-]
+    export OS=CYGWIN;;
 esac
-echo "OS = " $OS
 
 rm -rf SpecTcl/SpecTcl              # Due to bungled initial CVS load.
 
-if ( $#argv ==  1 ) then
-   set instdir = $1
-else if ( $#argv == 0 ) then
-   echo -n "Full path to installation directory: "
-   set instdir = $<
-else
-   echo "Usage: "
-   echo "    build.sh  [installation_directory_full_path]"
-   exit
-endif
+case $# in
+   0)
+     echo -n "Installation dir: "
+     read instdir;;
+   1)
+     instdir=$1;;
+   *)
+    echo "Usage:"
+    echo "  build.bash [installation_directory]"
+    exit;;
+esac
 
-set instdir=`(cd $instdir;pwd)`      # this trick allows relative paths.
+instdir=`(cd $instdir;pwd)`      # this trick allows relative paths.
 
 echo "Installing in" $instdir
 
