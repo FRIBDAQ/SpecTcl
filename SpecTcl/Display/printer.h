8e0b902b89acb29f5928443f9b1a2ed2302df5d3
Ron Fox <fox@nscl.msu.du>
2001-10-02 03:04:23 +0000

    Suport quick and dirty printing using Image Magick's import utility.

--- SpecTcl/Display/printer.h
+++ SpecTcl/Display/printer.h
@@ -12,15 +12,14 @@
 typedef enum {
                postscript,
 	       color_postscript,
-	       color_sixel,
-	       ascii
 	       } PrinterType;
 	       
 
 
 void Xamine_SetupPrinter(XMWidget *w, XtPointer user, XtPointer call);
-char *Xamine_GetPrinterTempFilename();
 char *Xamine_GetPrintCommand();
-char *Xamine_GetExpandePrintCommand(char *buf);
 PrinterType Xamine_GetPrinterType();
+
+void Xamine_printSelected(XMWidget* w, XtPointer user, XtPointer call);
+void Xamine_printAll(XMWidget* w, XtPointer user, XtPointer call);
 #endif
