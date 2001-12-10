1cd88504aff8248d9f36927675d29aef7106d268
Jason Venema <venema@nscl.msu.edu>
2001-12-10 17:59:07 +0000

    Removed class TranslatorFactory which had the same purpose for existence as class BufferFactory and was therefore unnecessary.

--- Events/BufferTranslator.h
+++ Events/BufferTranslator.h
@@ -2,12 +2,22 @@
   Interface for class BufferTranslator
 ******************************************************************************/
 
-#include <buffer.h>
-#include <histotypes.h>
-#include "TranslatorPointer.h"
+#ifndef __BUFFERTRANSLATOR_H
+#define __BUFFERTRANSLATOR_H
 
-#ifndef BUFFERTRANSLATOR_
-#define BUFFERTRANSLATOR_
+#ifndef __BUFFER_H
+#include <buffer.h>
+#endif
+
+#ifndef __TRANSLATORPOINTER_H
+#include "TranslatorPointer.h"
+#endif
+
+#ifndef __HISTOTYPES_H
+#include <histotypes.h>
+#endif
+
+#include <iostream.h>
 
 /*-----------------------------------------------------------------------------
   Definition of class BufferTranslator
@@ -17,87 +27,74 @@ class BufferTranslator
 {
  public:
   
-  UChar_t GetByte(UInt_t);
-  Short_t GetWord(UInt_t);
-  Long_t GetLongword(UInt_t);
+  UChar_t GetByte( UInt_t );
+  Short_t GetWord( UInt_t );
+  Long_t GetLongword( UInt_t );
 
   virtual Address_t getBuffer() = 0;
-  virtual void GetBlock(const Address_t, Int_t, Int_t) = 0;
+  virtual void GetBlock( const Address_t, Int_t, Int_t ) = 0;
 
   template <class T>
     T Get( Int_t );
 };
 
 template <class T>
-T BufferTranslator::Get(Int_t iOffset) {
-  iOffset *= sizeof( T );                 // Scale the offset
+T BufferTranslator::Get( Int_t iOffset ) {
+  iOffset *= sizeof(T);                 // Scale the offset
   T Result;
-  GetBlock(&Result, sizeof(Result), iOffset);  // Get the block
+  GetBlock( &Result, sizeof(T), iOffset );
   return Result;
 }
 
 /*-----------------------------------------------------------------------------
-  class SwappingBufferTranslator will contain byteswapped data
+  Definition of class SwappingBufferTranslator
 -----------------------------------------------------------------------------*/
 
 class SwappingBufferTranslator: public BufferTranslator
 {
+  Address_t m_pBuffer;  /*! A pointer to the buffer this holds */
+  
  public:
-  
-  SwappingBufferTranslator(Address_t pB = 0) {m_pBuffer = pB;}
-  
-  void GetBlock(const Address_t, Int_t, Int_t);
-  Address_t getBuffer() { return m_pBuffer; }
 
- private:
-  
-  Address_t m_pBuffer;
-  
+  // Default Constructor
+  SwappingBufferTranslator( Address_t pB = 0 ) {m_pBuffer = pB;}
+
+  // Accessor functions
+  void GetBlock( const Address_t, int, int );
+  Address_t getBuffer() { return m_pBuffer; } 
 };
 
 /*-----------------------------------------------------------------------------
-  class NonSwappingBufferTranslator non-byteswapped data
+  Definition of class NonSwappingBufferTranslator
 -----------------------------------------------------------------------------*/
 
 class NonSwappingBufferTranslator: public BufferTranslator
 {
+  Address_t m_pBuffer;   /*! A pointer to the buffer this holds */
+
  public:
 
-  NonSwappingBufferTranslator(Address_t pB = 0) {m_pBuffer = pB;}
+  // Default constrcutor
+  NonSwappingBufferTranslator( Address_t pB = 0 ) {m_pBuffer = pB;}
 
-  void GetBlock(const Address_t, Int_t, Int_t);
+  // Accessor functions
+  void GetBlock( const Address_t, int, int );
   Address_t getBuffer() { return m_pBuffer; }
-
- private:
-
-  Address_t m_pBuffer;
-
 };
 
 /*-----------------------------------------------------------------------------
-  Definition of factory class TranslatorFactory
+  Definition of factory class BufferFactory
 -----------------------------------------------------------------------------*/
-
-class TranslatorFactory
-{
- public: 
-  
-  enum Endian {little, big};
-  
-  static BufferTranslator* 
-    CreateTranslator(Address_t pBuffer, Endian eBufferOrdering);
-
-};
-
-//  MyEndianess returns the endianess of the running system
-TranslatorFactory::Endian MyEndianess();
 
 class BufferFactory
 {
  public:
-  
-  static BufferTranslator* CreateBuffer(Address_t pBuffer, Int_t Signature32);
+  enum Endian {little, big};
+  static BufferTranslator* 
+    CreateBuffer(Address_t pBuffer, Int_t Signature32);
 };
 
-#endif
+//  MyEndianess returns the endianess of the running system
+BufferFactory::Endian MyEndianess();
 
+#endif
