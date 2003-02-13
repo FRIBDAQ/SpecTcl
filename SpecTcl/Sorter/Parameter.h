76ed8095faab7f7473e62260b5bfca29734404d7
Ron Fox <fox@nscl.msu.du>
2003-02-13 16:23:42 +0000

    Add flag to indicate scaling is present or not.

--- SpecTcl/Sorter/Parameter.h
+++ SpecTcl/Sorter/Parameter.h
@@ -40,7 +40,7 @@ class CParameter  : public CNamedItem
   Float_t m_nLow;        // Low limit of the parameter scaling
   Float_t m_nHigh;       // High limit of the parameter scaling
   std::string m_sUnits;  // Optional units of this parameter scaling
-  
+  Bool_t  m_fScaled;	 // TRUE if no scale info provided.
 public:
   // Constructors with arguments.
 
@@ -48,7 +48,8 @@ public:
     CNamedItem(std::string("Undefined"), UINT_MAX),
     m_nScale(0),
     m_nLow(0.0),
-    m_nHigh(0.0)
+    m_nHigh(0.0),
+    m_fScaled(kfFALSE)
     { }
 
   CParameter(UInt_t am_nScale, 
@@ -56,7 +57,8 @@ public:
     CNamedItem(rName, nNumber),
     m_nScale (am_nScale),
     m_nLow(0.0),
-    m_nHigh(0.0)
+    m_nHigh(0.0),
+    m_fScaled(kfTRUE)
     { }
 
   CParameter(UInt_t am_nScale,
@@ -65,7 +67,8 @@ public:
     CNamedItem(rName, nNumber),
     m_nScale(am_nScale),
     m_nLow(nLow),
-    m_nHigh(nHigh)
+    m_nHigh(nHigh),
+    m_fScaled(kfTRUE)
     {
       m_sUnits.resize(strlen(am_sUnits.c_str()) + 2, 0);
       m_sUnits = am_sUnits;
@@ -81,6 +84,7 @@ public:
       m_nLow   = aCParameter.m_nLow;
       m_nHigh  = aCParameter.m_nHigh;
       m_sUnits = aCParameter.m_sUnits;
+      m_fScaled   = aCParameter.m_fScaled;
     }                                     
 
   //Operator= Assignment Operator
@@ -92,23 +96,35 @@ public:
       m_nLow   = aCParameter.m_nLow;
       m_nHigh  = aCParameter.m_nHigh;
       m_sUnits = aCParameter.m_sUnits;
+      m_fScaled   = aCParameter.m_fScaled;
 
       return *this;
     }       
 
   //Operator== Equality Operator
   int operator== (const CParameter& aCParameter) const
-  { return (
-	    (CNamedItem::operator== (aCParameter)) &&	    
-	    (m_nScale == aCParameter.m_nScale) &&
-	    (m_nLow == aCParameter.m_nLow) &&
-	    (m_nHigh == aCParameter.m_nHigh) &&
-	    (m_sUnits == aCParameter.m_sUnits)
-	    );
+  { 
+    if(!CNamedItem::operator==(aCParameter)) return kfFALSE;
+    if(m_sUnits != aCParameter.m_sUnits)     return kfFALSE;
+    if(m_fScaled  != aCParameter.m_fScaled)        return kfFALSE;
+    if(m_fScaled) {		// Scale factors only count if provided.
+      return ((m_nScale == aCParameter.m_nScale) &&
+	      (m_nLow == aCParameter.m_nLow) &&
+	      (m_nHigh == aCParameter.m_nHigh)
+	      );
+    }
+    else {
+      return kfTRUE;
+    }
+
+      
   }
   // Selectors:
 
 public:
+  Bool_t hasScale() const {
+    return m_fScaled;
+  }
   UInt_t getScale() const
   {
     return m_nScale;
@@ -130,19 +146,23 @@ public:
 protected:
   void setScale (UInt_t am_nScale)
   { 
-    m_nScale = am_nScale;
+    m_nScale  = am_nScale;
+    m_fScaled = kfTRUE;
   }
   void setLow(Float_t am_nLow) 
     {
-      m_nLow = am_nLow;
+      m_nLow    = am_nLow;
+      m_fScaled = kfTRUE;
     }
   void setHigh(Float_t am_nHigh) 
     {
-      m_nHigh = am_nHigh;
+      m_nHigh   = am_nHigh;
+      m_fScaled = kfTRUE;
     }
   void setUnits(std::string am_sUnits) 
     {
-      m_sUnits = am_sUnits;
+      m_sUnits  = am_sUnits;
+      m_fScaled = kfTRUE;
     }
 };
 
