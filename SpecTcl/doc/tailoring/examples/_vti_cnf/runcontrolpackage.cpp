933aaf1407477f1b090c706f5e75554300a9b0b2
Ron Fox <fox@nscl.msu.edu>
2003-08-25 16:25:29 +0000

    This commit was manufactured by cvs2svn to create branch 'SpecTcl-3-0_developmentreally'.

--- SpecTcl/doc/tailoring/examples/_vti_cnf/runcontrolpackage.cpp
+++
@@ -1,8 +0,0 @@
-vti_encoding:SR|utf8-nl
-vti_extenderversion:SR|2.0.2.1112
-vti_author:SR|FOX
-vti_modifiedby:SR|FOX
-vti_timecreated:TR|28 Jun 1999 09:00:43 -0400
-vti_timelastmodified:TR|28 Jun 1999 09:00:43 -0400
-vti_backlinkinfo:VX|tailoring/commands.htm
-vti_syncwith_localhost\\j\:\\public_html\\spectcl/j\:/public_html/spectcl:TR|28 Jun 1999 09:00:43 -0400
