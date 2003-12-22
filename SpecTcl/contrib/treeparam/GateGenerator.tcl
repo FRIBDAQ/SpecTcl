914e6b9e7bb8171db7a3a70285a9cef5fcb8e4ab
Ron Fox <fox@nscl.msu.du>
2003-12-22 17:19:26 +0000

    Incorporate Daniel's treeparameter update into SpecTcl

--- SpecTcl/contrib/treeparam/GateGenerator.tcl
+++ SpecTcl/contrib/treeparam/GateGenerator.tcl
@@ -1,6 +1,7 @@
 # GateGenerator.tcl
 # Author: D. Bazin
 # Date: September 2002
+# Version 1.2: November 2003
 
 proc SetupGateGenerator {parent} {
 	global topg
@@ -18,16 +19,21 @@ proc CreateGateGenerator {parent} {
 		frame $wname.r -borderwidth 2 -background lightyellow
 		frame $wname.r.t
 		frame $wname.r.b
+#		frame $wname.c
 		button $wname.l.create -text Create/Replace -command CreateGate -background lightyellow
 		entry $wname.l.name -width 15 -textvariable GateName -background  lightyellow
 		menubutton $wname.r.t.gate -text "Gate select" -background lightyellow
-		tk_optionMenu $wname.r.t.operation GateOperation AND OR NOT C2BAND
+		tk_optionMenu $wname.r.t.operation GateOperation And Or Not C2Band Slice Contour Band \
+		GammaBand GammaContour GammaSlice
 		$wname.r.t.operation configure -background lightyellow
-		entry $wname.r.b.depend -width 30 -textvariable GateDependency -background lightyellow
+		entry $wname.r.b.depend -width 80 -textvariable GateDependency -background lightyellow
 		button $wname.r.t.clear -text "Clear dependency" -background lightyellow -command {set GateDependency ""}
+#		button $wname.c.delete -text "Delete Selected" -command DeleteGate -width 15 -background lightblue
+#		button $wname.c.deleteall -text "Delete All" -command DeleteGateAll -width 15 -background lightblue
 		pack $wname.l.create $wname.l.name -expand 1 -fill x
 		pack $wname.r.t.gate $wname.r.t.clear $wname.r.t.operation -expand 1 -fill x -side left
 		pack $wname.r.b.depend -expand 1 -fill x
+#		pack $wname.c.delete $wname.c.deleteall -expand 1 -fill x -side top
 		pack $wname.r.t $wname.r.b -side top -expand 1 -fill x
 		pack $wname.l $wname.r -expand 1 -fill x -side left
 	pack $wname -expand 1 -fill x -anchor n
@@ -35,7 +41,7 @@ proc CreateGateGenerator {parent} {
 
 	set wname $topg.commands
 	frame $wname -borderwidth 2 -relief groove -background lightblue
-		button $wname.delete -text Delete -command DeleteGate -background lightblue
+		button $wname.delete -text "Delete Selected" -command DeleteGate -background lightblue
 		button $wname.deleteall -text "Delete All" -command DeleteGateAll -background lightblue
 		pack $wname.delete $wname.deleteall -expand 1 -fill x -side left
 	pack $wname -expand 1 -fill x -anchor n
@@ -46,12 +52,12 @@ proc CreateGateGenerator {parent} {
 	scrollbar $glist.vsb -orient vertical -command [list $glist.listbox yview] -background $glistcolor
 	scrollbar $glist.hsb -orient horizontal -command [list $glist.listbox xview] -background $glistcolor
 	set lbox $glist.listbox
-	mclistbox::mclistbox $lbox -fillcolumn name -height 15 -width 60 \
+	mclistbox::mclistbox $lbox -fillcolumn name -height 25 -width 60 \
 	-labelanchor w -selectmode extended -labelborderwidth 2 \
 	-xscrollcommand [list $glist.hsb set] -yscrollcommand [list $glist.vsb set] -background $glistcolor
 	$lbox column add name -label Name -width 15
 	$lbox column add type -label Type -width 15
-	$lbox column add depend -label Dependency -width 50
+	$lbox column add depend -label Dependency -width 60
 	$lbox label bind name <ButtonPress-1> "sort %W name"
 	$lbox label bind type <ButtonPress-1> "sort %W type"
 	$lbox label bind depend <ButtonPress-1> "sort %W depend"
@@ -77,14 +83,16 @@ proc CreateGateGenerator {parent} {
 
 proc GateGateCommand {theGate} {
 	global topg
+	if {[llength $theGate] > 1} {
+		set theGate "{$theGate}"
+	}
 	$topg.create.r.b.depend insert end "$theGate "
 }
 
 proc UpdateGateList {} {
 	global topg
 	$topg.glist.listbox delete 0 end
-	SendMessage "gate -list"
-	set theList [GetResponse]
+	set theList [gate -list]
 	foreach gate $theList {
 		set name [lindex $gate 0]
 		set id [lindex $gate 1]
@@ -115,11 +123,10 @@ proc DeleteGate {} {
 	if {[string match $selectedlist ""]} {return}
 	foreach index $selectedlist {
 		set gate [$lbox get $index]
-		SendMessage "gate -delete [lindex $gate 0]"
-		GetResponse
-#		$lbox delete $index $index
+		gate -delete [lindex $gate 0]
 	}
 	UpdateGateList
+	Modified
 }
 
 proc DeleteGateAll {} {
@@ -127,9 +134,15 @@ proc DeleteGateAll {} {
 	if {[string compare no [tk_messageBox -message \
 	"Are you sure you want to delete all gates?" -type yesno]] == 0} {return}
 	set lbox $topg.glist.listbox
-	SendMessage "gate -delete [$lbox.framename.listbox get 0 end]"
-	GetResponse
+	foreach g [$lbox.framename.listbox get 0 end] {
+		if {[llength $g] > 1} {
+			set g "{$g}"
+		}
+		gate -delete $g
+	}
+#	gate -delete [$lbox.framename.listbox get 0 end]
 	UpdateGateList
+	Modified
 }
 
 proc FillGateEntries {} {
@@ -145,6 +158,12 @@ proc FillGateEntries {} {
 		Or {$topg.create.r.t.operation.menu invoke 1}
 		Not {$topg.create.r.t.operation.menu invoke 2}
 		C2Band {$topg.create.r.t.operation.menu invoke 3}
+		Slice {$topg.create.r.t.operation.menu invoke 4}
+		Contour {$topg.create.r.t.operation.menu invoke 5}
+		Band {$topg.create.r.t.operation.menu invoke 6}
+		GammaBand {$topg.create.r.t.operation.menu invoke 7}
+		GammaContour {$topg.create.r.t.operation.menu invoke 8}
+		GammaSlice {$topg.create.r.t.operation.menu invoke 9}
 		default {$topg.create.r.t.operation.menu invoke 0}
 	}
 }
@@ -152,40 +171,78 @@ proc FillGateEntries {} {
 proc CreateGate {} {
 	global topg GateName GateDependency GateOperation
 	if {[string compare $GateName ""] == 0} {return}
+	if {[llength $GateName] > 1} {
+		set GateName "{$GateName}"
+	}
 	switch -- $GateOperation {
-		AND {
+		And {
 			if {[llength $GateDependency] < 2} {
 				tk_messageBox -icon error -message "Please enter at least two gates in the dependency"
 				return
 			}
-			SendMessage "gate $GateName * {$GateDependency}"
-			GetResponse
+			gate $GateName * $GateDependency
 		}
-		OR {
+		Or {
 			if {[llength $GateDependency] < 2} {
 				tk_messageBox -icon error -message "Please enter at least two gates in the dependency"
 				return
 			}
-			SendMessage "gate $GateName + {$GateDependency}"
-			GetResponse
+			gate $GateName + $GateDependency
 		}
-		NOT {
+		Not {
 			if {[llength $GateDependency] != 1} {
 				tk_messageBox -icon error -message "Please enter only one gate in the dependency"
 				return
 			}
-			SendMessage "gate $GateName - {$GateDependency}"
-			GetResponse
+			gate $GateName - $GateDependency
 		}
-		C2BAND {
+		C2Band {
 			if {[llength $GateDependency] != 2} {
 				tk_messageBox -icon error -message "Please enter only two gates in the dependency"
 				return
 			}
-			SendMessage "gate $GateName c2band {$GateDependency}"
-			GetResponse
+			gate $GateName c2band $GateDependency
+		}
+		Slice {
+			eval gate $GateName s "{$GateDependency}"
+		}
+		Contour {
+			set lg [llength $GateDependency]
+			set lp ""
+			for {set i 1} {$i < $lg} {incr i} {
+				lappend lp [lindex $GateDependency $i]
+			}
+			eval gate $GateName c "{[lindex [lindex $GateDependency 0] 0] [lindex [lindex $GateDependency 0] 1] {$lp}}"
+		}
+		Band {
+			set lg [llength $GateDependency]
+			set lp ""
+			for {set i 1} {$i < $lg} {incr i} {
+				lappend lp [lindex $GateDependency $i]
+			}
+			eval gate $GateName b "{[lindex [lindex $GateDependency 0] 0] [lindex [lindex $GateDependency 0] 1] {$lp}}"
+		}
+		GammaBand {
+			set lg [llength $GateDependency]
+			set lp ""
+			for {set i 1} {$i < $lg} {incr i} {
+				lappend lp [lindex $GateDependency $i]
+			}
+			eval gate $GateName gb "{[lindex [lindex $GateDependency 0] 0] [lindex [lindex $GateDependency 0] 1] {$lp}}"
+		}
+		GammaContour {
+			set lg [llength $GateDependency]
+			set lp ""
+			for {set i 1} {$i < $lg} {incr i} {
+				lappend lp [lindex $GateDependency $i]
+			}
+			eval gate $GateName gc "{[lindex [lindex $GateDependency 0] 0] [lindex [lindex $GateDependency 0] 1] {$lp}}"
+		}
+		GammaSlice {
+			eval gate $GateName gs "{$GateDependency}"
 		}
 	}
 	UpdateGateList
+	Modified
 }
 	
