a843482a1a029fdee5db37553b9e91b93f123752
Ron Fox <fox@nscl.msu.du>
2005-04-15 15:37:50 +0000

    Initial commit of user api header for the tree parameter package.

---
+++ SpecTcl/TreeParam/TreeParameter.h
@@ -0,0 +1,30 @@
+/*
+    This software is Copyright by the Board of Trustees of Michigan
+    State University (c) Copyright 2005.
+
+    You may use this software under the terms of the GNU public license
+    (GPL).  The terms of this license are described at:
+
+     http://www.gnu.org/licenses/gpl.txt
+
+     Author:
+             Ron Fox
+	     NSCL
+	     Michigan State University
+	     East Lansing, MI 48824-1321
+*/
+
+#ifndef __TREEPARAMETER_H
+#define __TREEPARAMETER_H
+
+#ifndef __CTREEPARAMETER_H
+#include <CTreeParameter.h>
+#endif
+
+#ifndef __CTREEVARIABLE_H
+#include <CTreeVariable.h>
+#endif
+
+
+
+#endif
