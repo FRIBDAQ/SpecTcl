299c42ec64cee3d6c72ac6a28b14d7e09063917e
Ron Fox <fox@nscl.msu.du>
2005-07-07 11:55:57 +0000

    Fix some casting issues on 64 bit archs.

--- SpecTcl/Display/refreshctl.cc
+++ SpecTcl/Display/refreshctl.cc
@@ -376,7 +376,7 @@ static void CheckAbort(XtPointer userd, XtIntervalId *id)
   /*
   ** First determine which pane we're dealing with.
   */
-  int index = (int)userd;
+  long index = (long)userd;
   int column = index  % WINDOW_MAXAXIS;
   int row    = column / WINDOW_MAXAXIS;
 
