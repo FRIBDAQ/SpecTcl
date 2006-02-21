928a6af563aa0de4de77f890419bf2e06f2b6aff
Ron Fox <fox@nscl.msu.du>
2006-02-21 19:54:48 +0000

    Fix more quoting issues.

--- Gui/newGui.tcl
+++ Gui/newGui.tcl
@@ -287,7 +287,7 @@ Please correct your input on the form}
             if {[pseudo -list $name] != ""} {
                 pseudo -delete $name;
             }
-            pseudo $name $depends $body
+            pseudo [list $name] $depends $body
 
             # Refresh the browser to show the new parameter:
 
