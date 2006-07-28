664655d48fda4e54a6ea343a3ada9255baba087c
Ron Fox <fox@nscl.msu.du>
2006-07-28 16:52:47 +0000

    Fix error in wysiwyg printing of zoomed displays

---
+++ SpecTcl/radware/CRWRead.cpp
@@ -0,0 +1,209 @@
+/*
+    This software is Copyright by the Board of Trustees of Michigan
+    State University (c) Copyright 2005.
+
+    You may use this software under the terms of the GNU public license
+    (GPL).  The terms of this license are described at:
+
+     http://www.gnu.org/licenses/gpl.txt
+
+     Author:
+             Ron Fox
+	     NSCL
+	     Michigan State University
+	     East Lansing, MI 48824-1321
+*/
+
+
+#include <config.h>
+#include "CRWRead.h"
+
+#include <TCLInterpreter.h>
+#include <SpecTcl.h>
+#include <Spectrum.h>
+
+#include "SpectrumFactory.h"
+#include <SnapshotSpectrum.h>
+
+#include <string>
+#include <string.h>
+#include <publib.h>
+
+#include "util.h"
+
+#ifdef HAVE_STD_NAMESPACE
+using namespace std;
+#endif
+
+static const int defaultMaxChans=8*1024;
+
+// Local static functions:
+
+static string
+Usage()
+{
+  string sresult = "Usage:\n";
+  sresult       += "    rwread ?-maxchans n? filename\n";
+  sresult       += "Where:\n";
+  sresult       += "    filename is the name of the file to read.\n";
+  sresult       += "    -maxchans if supplied is followed by an integer\n";
+  sresult       += "              that is the largest number of channels\n";
+  sresult       += "              to read.  This defaults to 8192";
+  return sresult;
+}
+
+/*!
+   Instantiate us... our command will always be rwread.
+*/
+CRWRead::CRWRead(CTCLInterpreter& interp) :
+  CTCLObjectProcessor(interp, string("rwread"))
+{}
+
+/*!
+   The destructor just establishes the chain to the base class.
+*/
+CRWRead::~CRWRead()
+{}
+
+/*!
+   Process the command:
+
+\verbatim
+   rwread ?-maxchans n? filename
+\endverbatim
+
+   This command reads a spectrum in radware .spe format from file
+into a SpecTcl 1d spectrum encapsulated in a snapshot spectrum.
+- -maxchans if present is the maximum number of channels the spectrum can
+   occupy.
+- filename is the name of the file to read.  If necessary, a .spe is appended
+  to give the filename an extension.
+*/
+int
+CRWRead::operator()(CTCLInterpreter& interp,
+		     vector<CTCLObject>& objv)
+{
+  // Need exactly 1  or exactly 3 arguments:
+  // (but add 1 for the command keyword).
+
+  if ((objv.size() != 2) && (objv.size() != 4)) {
+    string sresult = Usage();
+    CTCLObject oresult;
+    oresult.Bind(interp);
+    oresult = sresult;
+    Tcl_SetObjResult(interp.getInterpreter(), oresult.getObject());
+    return TCL_ERROR;
+
+  }
+  // Process the parameters:
+  // Filename is always last.
+  
+  string filename;
+  int    maxchans = defaultMaxChans;
+
+  if (objv.size() == 2) {
+    filename = objv[1];
+  }
+  else {
+    filename = objv[3];
+    string maxsw = objv[1];
+    if (maxsw != "-maxchans") {
+      string sresult = Usage();
+      CTCLObject oresult;
+      oresult.Bind(interp);
+      oresult = sresult;
+      Tcl_SetObjResult(interp.getInterpreter(), oresult.getObject());
+      return TCL_ERROR;
+    }
+    try {
+      objv[2].Bind(interp);	// Need this for conversiont ->int.
+      maxchans = objv[2];	// Fails if not integer.
+      if (maxchans < 0) throw "this"; // Force the catch block.
+    }
+    catch (...) {
+      string sresult = "Invalid maxchans parameter: ";
+      sresult       += objv[2];
+      sresult       += "\n";
+      sresult       += Usage();
+      CTCLObject oresult;
+      oresult.Bind(interp);
+      oresult = sresult;
+      Tcl_SetObjResult(interp.getInterpreter(), oresult.getObject());
+      return TCL_ERROR;
+    }
+  }
+  // At this point, filename has the name of the file to read.
+  // maxchans the maximum no. of channels.  We allocate the array of floats
+  // radware uses to hold the spectrum and read it in.
+
+  Float_t* chans = new Float_t[maxchans];
+  char     filenamecz[80];
+  char     specname[9];
+  int      actualSize;
+  memset(filenamecz, 0, sizeof(filenamecz));
+  memset(specname, 0, sizeof(specname));
+  strncpy(filenamecz, filename.c_str(), 79);
+
+  int status = read_spe_file(filenamecz, chans, specname, &actualSize, maxchans);
+
+  if (status != 0) {
+    delete []chans;
+    string sresult = "read_spe_file failed, see terminal output for more";
+      CTCLObject oresult;
+      oresult.Bind(interp);
+      oresult = sresult;
+      Tcl_SetObjResult(interp.getInterpreter(), oresult.getObject());
+      return TCL_ERROR;
+
+  }
+
+  // Now figure out what the spectrum name will be, and create the
+  // spectrum 
+
+  SpecTcl& api(*(SpecTcl::getInstance()));
+  int    uniquifier  = 1;
+  string spectclName = strrtrim(specname);
+  while (api.FindSpectrum(spectclName)) {
+    char spectclNamecz[100];
+    sprintf(spectclNamecz, "%s_%d", strrtrim(specname), uniquifier);
+    spectclName = spectclNamecz;
+    uniquifier++;
+  }
+
+
+  vector<UInt_t> dims;
+  vector<string> params;
+
+
+  dims.push_back(actualSize);
+  params.push_back("-deleted-");
+
+  CSpectrumFactory fact;
+  fact.ExceptionMode(kfFALSE);
+  CSpectrum* pSpectrum = fact.CreateSpectrum(spectclName, ke1D, keLong,
+					     params, dims);
+  
+  fact.ExceptionMode(kfTRUE);
+
+  // Load the spectrum with the channel values.
+
+  pSpectrum->Clear();
+  for (UInt_t i = 0; i < actualSize; i++) {
+    pSpectrum->set(&i, (UInt_t)chans[i]);
+  }
+
+  // Wrap in a snapshot spectrum...and add it to the histogrammer.
+
+  pSpectrum = new CSnapshotSpectrum(*pSpectrum, kfTRUE);
+  api.AddSpectrum(*pSpectrum);
+
+  // Deallocate dynamic memory and return.
+
+  delete []chans;
+  CTCLObject oresult;
+  oresult.Bind(interp);
+  oresult = spectclName;
+  Tcl_SetObjResult(interp.getInterpreter(), oresult.getObject());
+  
+  return TCL_OK;
+}
---
+++ SpecTcl/radware/CRWWrite.h
@@ -0,0 +1,63 @@
+/*
+    This software is Copyright by the Board of Trustees of Michigan
+    State University (c) Copyright 2005.
+
+    You may use this software under the terms of the GNU public license
+    (GPL).  The terms of this license are described at:
+
+     http://www.gnu.org/licenses/gpl.txt
+
+     Author:
+             Ron Fox
+	     NSCL
+	     Michigan State University
+	     East Lansing, MI 48824-1321
+*/
+#ifndef __CRWWRITE_H
+#define __CRWWRITE_H
+
+#ifndef __TCLOBJECTPROCESSOR_H
+#include <TCLObjectProcessor.h>
+#endif
+
+#ifndef __TCLOBJECT_H
+#include <TCLObject.h>
+#endif
+
+#ifndef __STL_VECTOR
+#include <vector>
+#ifndef __STL_VECTOR
+#define __STL_VECTOR
+#endif
+#endif
+
+
+// forward class defs:
+
+class CTCLInterpreter;
+
+
+
+/*!
+   This class implements a command processor to write a SpecTcl 1-d spectrum
+   in radware .spe form.  It makes use of elements of the radware package
+   that are redistributed by permission.
+*/
+class CRWWrite : public CTCLObjectProcessor
+{
+ public:
+  CRWWrite(CTCLInterpreter& interp);
+  virtual ~CRWWrite();
+
+
+  // Probably can't do the private unimplemented trick on loadable extensions.
+
+public:
+  virtual int operator()(CTCLInterpreter& interp,
+			 STD(vector)<CTCLObject>& objv);
+
+
+};
+
+
+#endif
