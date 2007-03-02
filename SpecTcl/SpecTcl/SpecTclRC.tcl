e3e478f566c40373af13106885d34d7d09db1eef
Ron Fox <fox@nscl.msu.du>
2007-03-02 00:02:00 +0000

    BZ 293 -  1. Remove NSCL license notice from tkcon.tcl; code is Jeff Hobbs (TclGuy)     reproduced both by explicit permission given during a bout of drinking     at one of the Tcl conferences and via the bourbonware license the code     is normally distributed under. 2. Butcher tkcon.tcl a bit further so that it uniquifies the SpecTcl    tk appname ensuring that multiple SpecTcl's all pop up their own tkcons.

--- SpecTcl/SpecTcl/SpecTclRC.tcl
+++ SpecTcl/SpecTcl/SpecTclRC.tcl
@@ -282,7 +282,6 @@
 #  Setup the standard scripted commandsin SpecTcl.
 #
 
-tk appname SpecTcl
 
 puts -nonewline "Loading SpecTcl gui..."
 source $SpecTclHome/Script/gui.tcl
@@ -308,4 +307,3 @@ puts -nonewline "Starting treeparamgui..."
 source $SpecTclHome/Script/SpecTclGui.tcl
 puts " Done"
 
-tk appname SpecTcl-[exec hostname]-[pid]
