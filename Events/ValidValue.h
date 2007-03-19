f72e24a3f9beb2111905a639614e0914d2f3be7a
Ron Fox <fox@nscl.msu.du>
2007-03-19 16:57:15 +0000

    Make Event's so they clear in O(1) time.

--- Events/ValidValue.h
+++ Events/ValidValue.h
@@ -1,37 +1,19 @@
 /*
-		    GNU GENERAL PUBLIC LICENSE
-		       Version 2, June 1991
+    This software is Copyright by the Board of Trustees of Michigan
+    State University (c) Copyright 2005.
 
- Copyright (C) 1989, 1991 Free Software Foundation, Inc.
-                       59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- Everyone is permitted to copy and distribute verbatim copies
- of this license document, but changing it is not allowed.
+    You may use this software under the terms of the GNU public license
+    (GPL).  The terms of this license are described at:
 
-			    Preamble
+     http://www.gnu.org/licenses/gpl.txt
 
-  The licenses for most software are designed to take away your
-freedom to share and change it.  By contrast, the GNU General Public
-License is intended to guarantee your freedom to share and change free
-software--to make sure the software is free for all its users.  This
-General Public License applies to most of the Free Software
-Foundation's software and to any other program whose authors commit to
-using it.  (Some other Free Software Foundation software is covered by
-the GNU Library General Public License instead.)  You can apply it to
-your programs, too.
-
-  When we speak of free software, we are referring to freedom, not
-price.  Our General Public Licenses are designed to make sure that you
-have the freedom to distribute copies of free software (and charge for
-this service if you wish), that you receive source code or can get it
-if you want it, that you can change the software or use pieces of it
-in new free programs; and that you know you can do these things.
-
-  To protect your rights, we need to make restrictions that forbid
-anyone to deny you these rights or to ask you to surrender the rights.
-These restrictions translate to certain responsibilities for you if you
-distribute c	     END OF TERMS AND CONDITIONS'
-
+     Author:
+             Ron Fox
+	     NSCL
+	     Michigan State University
+	     East Lansing, MI 48824-1321
 */
+
 //
 // ValidValue.h:
 //    Provides a class which contains a value and an indicator that the value
@@ -56,33 +38,52 @@ distribute c	     END OF TERMS AND CONDITIONS'
 template <class T>
 class CValidValue {
 private:
-  T m_Value;
-  Bool_t m_Valid;
+  T         m_Value;
+  ULong_t   m_nSerial;
+  ULong_t*  m_pSerial;		/* Allows for shared validity generation */
+  ULong_t   m_nLastSerial;      /* Serial number of last set.            */
+
 public:
   // Constructors and other cannonicals:
   //
-  CValidValue() : m_Valid(kfFALSE)
+  CValidValue() : 
+    m_nSerial(1),
+    m_pSerial(&m_nSerial),
+    m_nLastSerial(0)
   {
   }
-  CValidValue(T& initValue, Bool_t isValid = kfFALSE) : m_Value(initValue),
-							m_Valid(isValid)
+  CValidValue(T& initValue, Bool_t isValid = kfFALSE) : 
+    m_Value(initValue),
+    m_nSerial(1),
+    m_pSerial(&m_nSerial),
+    m_nLastSerial(isValid ? 0 : 1)
   {
   }
+
+  CValidValue(ULong_t* sharedSerial) :
+    m_pSerial(sharedSerial),
+    m_nLastSerial(*sharedSerial - 1)
+  {
+  }
+
   CValidValue(const CValidValue& rRHS) : 
     m_Value(rRHS.m_Value),
-    m_Valid(rRHS.m_Valid)
+    m_nSerial(rRHS.m_nSerial),
+    m_pSerial(rRHS.m_pSerial),
+    m_nLastSerial(rRHS.m_nLastSerial)
   {
   }
   CValidValue& operator=(const CValidValue& rRHS) {
     if(this != &rRHS) {
-      m_Value = rRHS.getValue();
-      m_Valid = rRHS.m_Valid;
+      m_Value        = rRHS.getValue();
     }
+    m_nLastSerial    = *m_pSerial;
     return *this;
   }
   int operator==(const CValidValue& rRhs) const {
-    if((!m_Valid) || (!rRhs.m_Valid)) return 0;	// Both must be valid.
-    return (m_Value == rRhs.m_Value);           // and equal.
+    if(!isValid() || !rRhs.isValid()) 
+      return 0;			// Both must be valid.
+    return (m_Value == rRhs.m_Value); // and equal.
   }
   int operator!=(const CValidValue& rRhs) const {
     return !operator==(rRhs);
@@ -92,13 +93,13 @@ public:
   //
 public:
   T getValue() const {
-    if(!m_Valid) {
+    if(!isValid()) {
       throw STD(string)("Attempted getValue of unset ValidValue object");
     }
     return m_Value;
   }
   Bool_t isValid() const {
-    return m_Valid;
+    return m_nLastSerial == *m_pSerial;
   }
   //
   // Mutators:
@@ -108,19 +109,21 @@ protected:
     m_Value = rNew;
   }
   void setValid(const Bool_t Valid) {
-    m_Valid = Valid;
+    m_nLastSerial = Valid ? *m_pSerial : *m_pSerial - 1;
   }
   // Object operations.
   //
 public:
-  void clear() { m_Valid = kfFALSE; }
+  void clear() {
+    m_nLastSerial = *m_pSerial - 1;
+  }
   CValidValue& operator=(const T& rRhs) {
     m_Value = rRhs;
-    m_Valid = kfTRUE;
+    m_nLastSerial = *m_pSerial;
     return *this;
   }
   operator T&() { 
-    if(!m_Valid) {
+    if(!isValid()) {
       throw STD(string)("Attempted getValue of unset ValidValue object");
     }
     return m_Value; 
