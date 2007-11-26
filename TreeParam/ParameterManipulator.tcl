ce1b3c034ad0893fc388ca8a87a95c96bc7708e1
Ron Fox <fox@nscl.msu.du>
2007-11-26 14:21:25 +0000

    BZ 327 - First part.  Rewrite ChangeSpectra so it actually          a. works for all 3.1 spectrum types.          b. attempts to work for all 3.2 types (must be checked with             3.2).

--- TreeParam/ParameterManipulator.tcl
+++ TreeParam/ParameterManipulator.tcl
@@ -136,64 +136,226 @@ proc ListParameterArray {id} {
 	}
 	return $pList
 }
+#
+# changeAxisLimits
+#   Given a spectrum axis definition (could be more than one axis def
+#   modifes one of the axis definitions to have a new low/high value.
+#
+# Parameters:
+#    axes     - Old axis definitions.
+#    which    - Which axis to change.
+#    low      - new low value
+#    high     - new high value
+# Returns:
+#    new axis definitions.
+#
+proc changeAxisLimits {axes which low high} {
+    set axis [lindex $axes $which]
+    set axis [lreplace $axis 0 1 $low $high]
+    set axes [lreplace $axes $which $which $axis]
 
+    return $axes
+}
+
+#
+#  Change the definition of a spectrum after a dependent parameter
+#  has been modified.  What this should mean is that for every parameter
+#  that has been modified (remember you can modify an array of parameters),
+#  Spectra that depend on at least one of those parameters will be re-created
+#  with axis specifications for the modified parameters modifed to reflect the
+#  parameter's new low/high limits (same number of channels as before I'm presuming).
+#
+#  Parameters:
+#    id    - The id of the parameter that has been modified.
+#  Implicit inputs:
+#    SpecTcl parameter list,
+#    parameter(Array) is true if the array checkbox is lit indicating
+#                     the base of the parameter should be treated as an
+#                     array for which all parameters must be changed.
+#
 proc ChangeSpectra {id} {
-	global parameter
-	set theList [spectrum -list]
-	set sList ""
-	set nList ""
-	foreach s $theList {
-		set sName [lindex $s 1]
-		set parList [lindex $s 3]
-		if {$parameter(Array) && [IsParameterArray $id]} {
-			foreach p [ListParameterArray $id] {
-				if {[lsearch $parList $p] != -1} {
-					lappend sList $sName
-					lappend nList [lsearch $parList $p]
-					break
-				}
-			}
-		} else {
-			if {[lsearch $parList $parameter(Name$id)] != -1} {
-				lappend sList $sName
-				lappend nList [lsearch $parList $parameter(Name$id)]
-			}
+    global parameter
+    set theList [spectrum -list]
+    set sList ""
+    set nList ""
+    # Create the list of parameters that will be modified:
+    #
+    
+    if {$parameter(Array) && [IsParameterArray $id]} {
+	set parameterList [ListParameterArray $id]
+    } else {
+	set parameterList $parameter(Name$id)
+    }
+    foreach s $theList {
+	set sName [lindex $s 1]
+	set parList [lindex $s 3]
+	foreach p $parameterList {
+	    if {[lsearch $parList $p] != -1} {
+		lappend sList $sName
+		break
+	    }
+	}
+    }
+
+    
+    # 
+    #  At this point, slist is the list of spectra that will be changed if the
+    #  user confirms.
+    
+    
+    set response [tk_messageBox -icon warning -type yesno -parent .gui\
+		      -message "The following spectra will be erased and replaced:\n$sList\nDo you wish to continue?"]
+    if {[string equal $response no]} {
+	return
+    }
+    
+    # Iterate through sList and modify each spectrum according to the type
+    # and set of parameters that match.
+    # There are several sorts of spectra:
+    #   Spectra for which the parameter index indicates the axis index to modify
+    #  (1-d, 2-d)
+    #  Spectra that don't get modified at all:
+    #    Currently only the strip chart.
+    #  Spectra where if there is any parameter match, all axes are modified:
+    #   g1, g2, s
+    #  Spectra that have to be handled specially:
+    #   m2, gd, b
+    #
+
+    foreach spectrum $sList {
+	set definition [lindex [spectrum -list $spectrum] 0];
+	set name       [lindex $definition 1]
+	set type       [lindex $definition 2]
+	set parameters [lindex $definition 3]
+	set axes       [lindex $definition 4]
+	set dataType   [lindex $definition 5]
+
+	switch -exact --  $type {
+	    1 - 2 {
+		# The index of the parameter match defines which axis is modified.
+                # both axes could conceivably change!
+		foreach p $parameterList {
+		    set which [lsearch -exact $parameters $p]
+		    if {$which >= 0} {
+			puts "Matched $p to $which" 
+			set parameterDef [lindex [treeparameter -list $p] 0]
+			puts "parameter def $parameterDef"
+			set low [lindex $parameterDef 2]
+			set high [lindex $parameterDef 3]
+			puts "low, high: $low $high"
+			puts "axes before: $axes"
+			set axes [changeAxisLimits $axes $which $low $high]
+			puts "axes after: $axes"
+
+		    }
 		}
-	}
-	set response [tk_messageBox -icon warning -type yesno -parent .gui\
-	-message "The following spectra will be erased and replaced:\n$sList\nDo you wish to continue?"]
-	if {[string equal $response no]} {
-		return
-	}
-	SetParameter $id
-	set index 0
-	foreach s $sList {
-		set ss [spectrum -list $s]
-		set sType [lindex $ss 2]
-		set sParam [lindex $ss 3]
-		set sReso [lindex $ss 4]
-		set sData [lindex $ss 5]
-		set sNew ""
-		for {set i 0} {$i < [llength $sReso]} {incr i} {
-			if {$i == [lindex $nList $index]} {
-				set bins [lindex [lindex $sReso $i] 2]
-				lappend sNew [list $parameter(Start$id) $parameter(Stop$id) $bins]
+	    }
+	    g1 - g2 - s {
+		# For the first match, all axes are changed.
+		# all axes will change, first match is ok because if its an array parameter set,
+		# all parmeters will get the same limits
+		#
+		set axisCount [llength $axes]
+		foreach p $parameterList {
+		    set which [lsearch -exact $parameters $p]
+		    if {$which >= 0} {
+			set parameterDef [lindex [treeparameter -list $p] 0]
+			set low          [lindex $parameterDef 2]
+			set high         [lindex $parameterDef 3]
+
+			# Change all axes:
+
+			for {set i 0} {$i < $axisCount} {incr i} {
+			    set axes [changeAxisLimits $axes $i $low $high]
+			}
+			break
+		    }
+		}
+	    }
+	    m2 {
+		# The parameter list are x/y pairs.  Therefore
+		# we go for all matches, even matches modify the x axis
+		# odd matches the y.
+
+		foreach p $parameterList {
+		    set which [lsearch -exact $parameters $p]
+		    if {$which >= 0} {
+			set axis [expr {$which % 2}];   # 0 or 1...
+			set parameterDef [lindex [treeparameter -list $p] 0]
+			set low          [lindex $parameterDef 2]
+			set high         [lindex $parameterDef 3]
+			set axes         [changeAxisLimits $axes $axis $low $high]
+		    }
+		}
+	    }
+	    gd {
+		# Gamma deluxe:  This has two lists of parameters.
+		# matches in the first list change x.
+		# matches in the second list change y.
+
+		set xParameters [lindex $parameters 0]
+		set yParameters [lindex $parameters 1]
+		foreach p $parameterList {
+		    set which [lsearch -exact $xParameters $p]
+		    if {$which >= 0} {
+			set parameterDef [lindex [treeparameter -list $p] 0]
+			set low          [lindex $parameterDef 2]
+			set high         [lindex $parameterDef 3]
+			set axes         [changeAxisLimits $axes 0 $low $high]
+		    }
+
+		    set which [lsearch -exact $yParameters $p]
+		    if {$which >= 0} {
+			set parameterDef [lindex [treeparameter -list $p] 0]
+			set low          [lindex $parameterDef 2]
+			set high         [lindex $parameterDef 3]
+			set axes         [changeAxisLimits $axes 1 $low $high]
+		    }
+
+		}
+	    }
+	    b {
+		# Bitmask spectrum.  The low/high are determined by the
+		# log2 of the low/high values.. e.g. 
+		# a range of 4 - 32 gives a spectrum range of
+		# 2-5   can't take the log of <= 0, those are mapped to 0.
+		#
+		# we use log2(x) = loge(x)/loge(2)
+		# non integer weirdness is truncated on the low end and incremented on the high end
+		#
+		foreach p $parameterList {
+		    set which [lsearch -exact $parameters $p]
+		    if {$which == 0} {
+			set parameterDef [lindex [treeparameter -list $p] 0]
+			set low          [lindex $parameterDef 2]
+			set high         [lindex $parameterDef 3]
+			if {$low <= 0} {
+			    set low 0
 			} else {
-				lappend sNew [lindex $sReso $i]
+			    set low [expr {int(log($low)/log(2))}]
 			}
+			if {$high <= 0} {
+			    set high 0
+			} else {
+			    set high [expr {int(ceil(log($high)/log(2)))}]
+			}
+			set axes [changeAxisLimits $axes 0 $low $high]
+			break
+		    }
 		}
-		set gate [lindex [lindex [lindex [apply -list $s] 0] 1] 0]
-		spectrum -delete $s
-		spectrum $s $sType $sParam $sNew $sData
-		if {![string equal $gate -TRUE-]} {
-			apply $gate $s
-		}
-		puts "Spectrum $s replaced"
-		incr index
+	    }
 	}
-	UpdateSpectrumList
-	sbind -all
-	Modified
+	# Set the new spectrum definition:
+
+	spectrum -delete $name
+	spectrum $name $type $parameters $axes $dataType
+
+    }
+    
+    
+    UpdateSpectrumList
+    sbind -all
+    Modified
 }
 
 proc LoadParameter {id} {
