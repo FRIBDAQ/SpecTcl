1e80bbae64daba18a36605b23d74687fdf5cead3
Ron Fox <fox@nscl.msu.du>
2008-01-29 20:32:11 +0000

    Fix command names back to what they were

--- CRWWrite.cpp
+++ CRWWrite.cpp
@@ -32,7 +32,7 @@ using namespace std;
    Instantiate us... our command will just be rwwrite.
 */
 CRWWrite::CRWWrite(CTCLInterpreter& interp) :
-  CTCLObjectProcessor(interp, string("RDWwrite"))
+  CTCLObjectProcessor(interp, string("rwwrite"))
 {}
 
 /*!
