6abc4489d932f3f659ab2e0a50e44416d3bf437c
Ron Fox <fox@nscl.msu.du>
2009-08-07 10:43:42 +0000

    All of the files needed are defined and some of them have been coded but no compilations yet done.

---
+++ plugins/const/CConstData.cpp
@@ -0,0 +1,239 @@
+/*
+    This software is Copyright by the Board of Trustees of Michigan
+    State University (c) Copyright 2008
+
+    You may use this software under the terms of the GNU public license
+    (GPL).  The terms of this license are described at:
+
+     http://www.gnu.org/licenses/gpl.txt
+
+     Author:
+             Ron Fox
+	     NSCL
+	     Michigan State University
+	     East Lansing, MI 48824-1321
+*/
+#include <config.h>        // SpecTcl configuration file.
+#include "CConstData.h"    // my class.
+#include <SpecTcl.h>       // SpecTcl API.
+
+
+using namespace std;
+
+
+// Class level data:
+
+CConstData*  CConstData::m_pInstance(0); // Singleton pointer.
+
+///////////////////////////////////////////////////////////////////////
+/*
+ * Constructor is private:
+ */
+CConstData::CConstData()
+{
+  m_pInstance = this;
+}
+
+/*!
+   Obtain the instance pointer:
+   @return CConstData&
+   @retval Pointer to the single instance (which is crated if necessary).
+
+*/
+CConstData&
+CConstData::getInstance()
+{
+  if (!m_pInstance) {
+    new CConstData();		// Stores the instance pointer.
+  }
+  return *m_pInstance;
+}
+
+//////////////////////////////////////////////////////////////////////
+/*!
+   Adds an and parameter.  
+   @param name (std::string)  The name of the output parameter.
+   @param value (double)      The output value to assign the parameter if
+                              all the independent parameters got a value this event.
+   @param inputs (std::vector<std::string>)  The independent parameters.
+   
+   @throw string - output paramter exists.
+   @throw string - One of the input parameters does not exist.
+*/
+void
+CConstData::addAndParameter(std::string name, double value, 
+			    std::vector<std::string> inputs)
+{
+  addParameter(m_andParameters, 
+	       std::string name, double value, 
+	       std::vector<std::string> inputs);
+}
+/*!
+   Adds an or parameter.
+   Adds an and parameter.  
+   @param name (std::string)  The name of the output parameter.
+   @param value (double)      The output value to assign the parameter if
+                              any of the independent parameters got a value this event.
+   @param inputs (std::vector<std::string>)  The independent parameters.
+   
+   @throw string - output paramter exists.
+   @throw string - One of the input parameters does not exist.
+*/
+void
+ConstData:: addOrParameter(std::string name,  double value, 
+			   std::vector<std::string> inputs)
+{
+  addParameter(m_orParameters,
+	       std::string name, double value, 
+	       std::vector<std::string> inputs);
+}
+
+/*!
+   Removes an existing parameter.
+   - The parameter is removed from the appropriate list.
+   - The parameter is removed from the dictionary.
+   - The SpecTcl parameter is destroyed via the api.
+
+   @param name (std::string) Name of the parameter to destroy.
+
+   @throw string - If parameter is not a const parameter.
+*/
+void
+ConstData::deleteParameter(std::string name)
+{
+  DictionaryIterator p = m_dictionary.find(name);
+  if (p == m_dictionary.end()) {
+    throwParameterNotConst(name);
+  }
+  else {
+    p->second.s_pList.erase(p->second.s_pParam); // Erase from list.
+    m_dictionary.erase(p);
+    SpecTcl* pApi = SpecTcl::getInstance();
+    pApi->RemoveParameter(name);
+    
+  }
+
+}
+//////////////////////////////////////////////////////////////////////
+
+
+
+CConstData::ParameterIterator
+CConstData::andBegin()
+{
+  return m_andParameters.begin();
+}
+
+CConstData::ParameterIterator
+CConstData::andEnd()
+{
+  return m_andParameterse.end();
+}
+
+CConstData::ParameterIterator
+CConstData::orBegin()
+{
+  return m_orParameters.begin();
+}
+
+CConstData::ParameterIterator
+CConstData::orEnd()
+{
+  return m_orParameters.end();
+}
+
+///////////////////////////////////////////////////////////////////////
+
+
+/*
+ *
+ *  Adds a new parameter
+ *  This is common code for the addAndParameter and 
+ *  addOrParameter methods.  The call sequence is the same, however the
+ *  first parameter is a reference to the list in which the parameter will be
+ *  added.
+ */
+void
+CConstData::addParameter(ParameterDefinitions& which, 
+			 std::string name, double value, 
+			 std::vector<std::string> inputs)
+{
+  SpecTcl*            pApi     = SpecTcl::getInstance();
+  ParameterDefinition def;
+  DictionaryEntry     dict;
+  CParameter*         pParameter;
+
+  // First validate the output and input parameters.
+  // if they are not valid, call the appropriate thrower method:
+
+  pParameter  = pApi=>FindParameter(name); 
+  if(pParameter) {
+    throwParameterExists(name);
+  }
+  for (int i =0; i < inputs.size(); i++) {
+    pParameter = pApi->FindParameter(inputs[i]);
+    if (!pParameter) {
+      throw ParameterDoesNotExist(inputs[i]);
+    }
+    def.s_inputParameters.push_back(pParameter->getNumber());
+  }
+  // It's legal to make the parameter.  We already have the list of
+  // input parameters.  make the output parameter:
+
+  def.s_outParameterId = pApi->AssignParameterId();
+  def.outValue         = value;
+  pApi->AddParameter(name,def.s_outParameterId, string(""));
+
+  which.push_back(def);
+
+  dict.s_pList  = &which;
+  dict.s_pParam = which.back();
+ 
+}
+
+/*
+ * Throw a string exception that says a parameter exists.
+ * Parameters:
+ *   name - name of the  parameter.
+ */
+void
+CConstData::throwParameterExists(std::string name)
+{
+  string exception = embedName("Cannot use", name,
+			       "as a const because it is a previously defined parameter");
+  throw exception;
+}
+/*
+ * Throw the string exception appropriate to a nonexistent parameter.
+ */
+void
+CConstData::throwParameterDoesNotExist(std::string name)
+{
+  string exception embedName("There is no parameter named",name, 
+			     "defined in SpecTcl");
+  throw exception;
+}
+/*
+ * Throw the string exception appropriate to a parameter not being an existing
+ * const parameter.
+ */
+void
+CConstData::throwParameterNotConst(std::string name)
+{
+  string exception embedName("", name,
+			     "is not an existing 'const' parameter");
+  throw exception;
+}
+
+/*
+ * Glue three strings together separated by space:
+ */
+std::string
+CConstData::embedName(const char* prefix, std::string name, const char* suffix)
+{
+  string output(prefix);
+  prefix += ' ';
+  prefix += name;
+  prefix += ' ';
+  prefix += suffix;
+}
---
+++ plugins/const/CConstData.h
@@ -0,0 +1,126 @@
+#ifndef __CCONSTDATA_H
+#define __CCONSTDATA_H
+/*
+    This software is Copyright by the Board of Trustees of Michigan
+    State University (c) Copyright 2008
+
+    You may use this software under the terms of the GNU public license
+    (GPL).  The terms of this license are described at:
+
+     http://www.gnu.org/licenses/gpl.txt
+
+     Author:
+             Ron Fox
+	     NSCL
+	     Michigan State University
+	     East Lansing, MI 48824-1321
+*/
+
+#ifndef __STL_VECTOR
+#include <vector>
+#ifndef __STL_VECTOR
+#define __STL_VECTOR
+#endif
+#endif
+
+#ifndef __STL_STRING
+#include <string>
+#ifndef __STL_STRING
+#define __STL_STRING
+#endif
+#endif
+
+#ifndef __STL_MAP
+#include <map>
+#ifndef __STL_MAP
+#define __STL_MAP
+#endif
+#endif
+
+/*!
+   This class contains the data used to drive the const  event processor.
+   The data maintenance functions are called by the command processor or
+   are available to other software given that the plugin is loaded.
+
+   The idea is that there are two lists of parameter definitions, One for
+   and parameters one for or parameters.  There's a common parameter dictionary
+   that contains iterators into the appropriate list.  This allows for
+   parameter deletion regardless of which list the parameter is in.
+
+*/
+class CConstData
+{
+  // Data type definitions:
+
+public:
+  // 
+  // Parameter definition in one of the lists:
+  //
+  typedef struct _ParameterDefinition {
+    int                s_outParameterId;        // Output parameter id.
+    double             s_outValue;              // Value to stor in output parameter.
+    std::vector<int>   s_inputParameters;       // Independent parameter ids.
+  } ParameterDefinition;
+
+
+  typedef std::vector<ParameterDefinition> ParameterDefinitions;
+  typedef ParameterDefinitions::iterator   ParameterIterator;
+
+  typdef struct _DictionaryEntry {
+    ParameterDefinitions*  s_pList;
+    ParameterIterator      s_pParam;
+  } DictionaryEntry;
+
+  typedef std::map<std::string, DictionaryEntry> ParameterDictionary;
+  typedef ParameterDictionary::iterator    DictionaryIterator;
+
+  // Managed data:
+
+private:
+  ParameterDefinitions    m_andParameters;
+  ParameterDefinitions    m_orParameters;
+  ParameterDictionary     m_dictionary;
+
+  // Singleton instance
+
+  static CConstData*     m_pInstance;
+
+  // Constructors...and canonicals.. remember this is a singleton:
+
+private:
+  CConstData();
+
+  // Access to the singleton:
+
+public:
+  static CConstData& getInstance();
+
+  // Parameter maintenance (used by the command processor typically)
+
+public:
+  void addAndParameter(std::string name, double value, std::vector<std::string> inputs);
+  void addOrParameter(std::string name,  double value, std::vector<std::string> inputs);
+  void deleteParameter(std::string name);
+
+  // Iteration (used by the event processor typically)
+
+  ParameterIterator andBegin();
+  ParameterIterator andEnd();
+
+  ParameterIterator orBegin();
+  ParameterIterator orEnd();
+
+
+  // Helpers and factorizers:
+private:
+  void addParameter(ParameterDefinitions& which, 
+		    std::string name, double value, std::vector<std::string> inputs);
+  void throwParameterExists(std::string name);
+  void throwParameterDoesNotExist(std::string name);
+  void throwParameterNotConst(std::string name);
+  std::string embedName(const char* prefix, std::string name, const char* suffix);
+
+};
+
+
+#endif
