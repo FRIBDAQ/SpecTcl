cbe0dc5804d2fad9fec5c03e1725d490015f50c9
Ron Fox <fox@nscl.msu.du>
2011-04-08 20:51:27 +0000

    Remove stray character that accidently got stuck in there.

--- nextgen/primitives/spectcl_wsspectest.c
+++ nextgen/primitives/spectcl_wsspectest.c
@@ -258,7 +258,7 @@ int main(void)
   int  failures;
 
   Suite* s = suite_create("spectcl_workspace_spectra");
-  SRunner* sr = srunner_create(s);3
+  SRunner* sr = srunner_create(s);
   TCase*   tc_spectra = tcase_create("spectra");
 
   tcase_add_checked_fixture(tc_spectra, setup, teardown);
