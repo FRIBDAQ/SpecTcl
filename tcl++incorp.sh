78275dbed9e428f53d5eb4a460360b5b468c3932
Ron Fox <fox@nscl.msu.du>
2013-05-10 20:02:29 +0000

    libtcl incorporation script

---
+++ tcl++incorp.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+##
+#  Shell script to do an export of a specific tag of the libtcl
+#  Usage:
+#    tcl++incorp.sh tag 
+#
+#  The export is done to the libtcl directory.
+#
+#
+#  The idea is that libtcl/libexception could be or not be centrally
+#  installed, but if not, built from the export directory.
+#
+# 
+
+baseURL="https://svn.code.sf.net/p/nscldaq/code/tags"
+
+tag="$1"
+
+URL="$baseURL/$tag"
+
+echo $URL
+
+svn co $URL libtcl
+
