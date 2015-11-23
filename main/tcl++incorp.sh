46972b953867b7c4877791cd9890f0a075b88a5b
Ron Fox <fox@nscl.msu.edu>
2015-11-23 10:36:39 -0500

    Modified build harnesses for git.

--- main/tcl++incorp.sh
+++ main/tcl++incorp.sh
@@ -11,17 +11,14 @@
 #  The idea is that libtcl/libexception could be or not be centrally
 #  installed, but if not, built from the export directory.
 #
-# Last used with libtclplus-v2.0-002
+# Last used with libtclplus-v2.0-000
 
-baseURL="https://svn.code.sf.net/p/nscldaq/code/tags"
+baseURL="git://git.code.sf.net/p/nscldaq/git"
 
 tag="$1"
 
-URL="$baseURL/$tag"
+rm -rf libtcl 
 
-echo $URL
-
-svn export $URL libtcl
-
+git clone $baseURL libtcl
+(cd libtcl; git checkout tags/$1)
 (cd libtcl; autoreconf -i)
-
