6035a588a366acf618397da664f06a28a2c46c87
Jeromy Tompkins <tompkins@nscl.msu.edu>
2015-12-18 16:42:36 -0500

    Adds BindList tests

--- main/SpecTcl/SpectrumPackageTests.cpp
+++ main/SpecTcl/SpectrumPackageTests.cpp
@@ -17,6 +17,7 @@
 
 #include <vector>
 #include <string>
+#include <iterator>
 
 using namespace std;
 
@@ -25,6 +26,8 @@ class SpectrumPackageTests : public CppUnit::TestFixture
     CPPUNIT_TEST_SUITE(SpectrumPackageTests);
     CPPUNIT_TEST(create_0);
     CPPUNIT_TEST(bindAll_0);
+    CPPUNIT_TEST(bindList_0);
+    CPPUNIT_TEST(bindList_1);
     CPPUNIT_TEST_SUITE_END();
 
 private:
@@ -44,6 +47,26 @@ public:
         m_pDM->setCurrentDisplay("test");
         gpDisplayInterface = m_pDM;
         m_pPkg    = new CSpectrumPackage(m_pInterp, m_pSorter, m_pDM);
+
+        createSpectra();
+
+    }
+
+    void createSpectra() {
+        CTCLResult result(m_pInterp, true);
+
+        m_pSorter->AddParameter("test.0", 0, 1, 0, 100., "arb");
+
+        vector<string> params(1); params[0] = "test.0";
+        vector<UInt_t> nbins(1); nbins[0] = 100;
+        vector<Float_t> lows(1); lows[0] = 0;
+        vector<Float_t> highs(1); highs[0] = 100;
+
+        m_pPkg->CreateSpectrum(result, "test1", "1",
+                               params, nbins, lows, highs, "long");
+        m_pPkg->CreateSpectrum(result, "test2", "1",
+                               params, nbins, lows, highs, "long");
+
     }
 
     void tearDown() {
@@ -57,19 +80,17 @@ public:
     void create_0 () {
         CTCLResult result(m_pInterp, true);
 
-        m_pSorter->AddParameter("test.0", 0, 1, 0, 100., "arb");
-
         vector<string> params(1); params[0] = "test.0";
         vector<UInt_t> nbins(1); nbins[0] = 100;
         vector<Float_t> lows(1); lows[0] = 0;
         vector<Float_t> highs(1); highs[0] = 100;
 
-        m_pPkg->CreateSpectrum(result, "test", "1",
+        m_pPkg->CreateSpectrum(result, "unique", "1",
                                params, nbins, lows, highs, "long");
-        EQMSG("Spectrum creation succeeded", string("test"), result.getString());
+        EQMSG("Spectrum creation succeeded", string("unique"), result.getString());
 
         SpecTcl*    pApi = SpecTcl::getInstance();
-        CSpectrum* pSpec = pApi->FindSpectrum("test");
+        CSpectrum* pSpec = pApi->FindSpectrum("unique");
         std::vector<UInt_t> resolution;
         pSpec->GetResolutions(resolution);
         EQMSG("N bins correct", nbins[0], resolution[0]);
@@ -81,17 +102,6 @@ public:
     void bindAll_0 () {
         CTCLResult result(m_pInterp);
 
-        m_pSorter->AddParameter("test.0", 0, 1, 0, 100., "arb");
-
-        vector<string> params(1); params[0] = "test.0";
-        vector<UInt_t> nbins(1); nbins[0] = 100;
-        vector<Float_t> lows(1); lows[0] = 0;
-        vector<Float_t> highs(1); highs[0] = 100;
-
-        m_pPkg->CreateSpectrum(result, "test1", "1",
-                               params, nbins, lows, highs, "long");
-        m_pPkg->CreateSpectrum(result, "test2", "1",
-                               params, nbins, lows, highs, "long");
 
         m_pPkg->BindAll(result);
 
@@ -106,8 +116,38 @@ public:
 
     void bindList_0 () {
 
+        CTCLResult result(m_pInterp);
+
+        std::vector<string> names(2);
+        names[0] = "test1";
+        m_pPkg->BindList(result, names);
+
+        CDisplay* pDisplay = m_pDM->getCurrentDisplay();
+        int test1Id = pDisplay->FindDisplayBinding("test1");
+        int test2Id = pDisplay->FindDisplayBinding("test2");
+
+        ASSERT(test1Id != -1);
+        ASSERT(test2Id == -1);
 
     }
+
+    void bindList_1 () {
+
+        CTCLResult result(m_pInterp);
+
+        CSpectrum* pSpec1 = m_pSorter->FindSpectrum("test1");
+        std::vector<UInt_t> ids(1);
+        ids[0] = pSpec1->getNumber();
+        m_pPkg->BindList(result, ids);
+
+        CDisplay* pDisplay = m_pDM->getCurrentDisplay();
+        int test1Id = pDisplay->FindDisplayBinding("test1");
+        int test2Id = pDisplay->FindDisplayBinding("test2");
+
+        ASSERT(test1Id != -1);
+        ASSERT(test2Id == -1);
+    }
+
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION(SpectrumPackageTests);
