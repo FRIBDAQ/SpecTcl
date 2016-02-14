0311fc4e2686cabdb28ed71a356ba14b4b9b81c7
Jeromy Tompkins <tompkins@nscl.msu.edu>
2016-02-14 13:58:40 -0500

    Stubs out CTestXamineShMem

--- main/Core/TestXamineShMem.cpp
+++ main/Core/TestXamineShMem.cpp
@@ -3,3 +3,49 @@
 CTestXamineShMem::CTestXamineShMem()
 {
 }
+
+void CTestXamineShMem::setManaged(bool value) {}
+
+bool CTestXamineShMem::isManaged() const {}
+
+void CTestXamineShMem::attach() {}
+void CTestXamineShMem::detach() {}
+
+size_t CTestXamineShMem::getSize() const {}
+
+UInt_t CTestXamineShMem::addSpectrum(CSpectrum& , CHistogrammer& ) {}
+void CTestXamineShMem::removeSpectrum(UInt_t slot, CSpectrum& ) {}
+
+DisplayBindings CTestXamineShMem::getDisplayBindings() const {}
+
+CXamineSpectrum& CTestXamineShMem::operator[](UInt_t n) {}
+DisplayBindingsIterator CTestXamineShMem::displayBindingsBegin() {}
+DisplayBindingsIterator CTestXamineShMem::displayBindingsEnd() {}
+
+UInt_t CTestXamineShMem::displayBindingsSize() const {}
+Int_t CTestXamineShMem::findDisplayBinding(std::string name) {}
+Int_t CTestXamineShMem::findDisplayBinding(CSpectrum& rSpectrum) {}
+
+void CTestXamineShMem::addFit(CSpectrumFit& fit) {}
+void CTestXamineShMem::deleteFit(CSpectrumFit& fit) {}
+
+std::string CTestXamineShMem::createTitle(CSpectrum& rSpectrum,
+                        UInt_t maxLength,
+                        CHistogrammer &rSorter) {}
+UInt_t CTestXamineShMem::getTitleSize()  const {}
+void CTestXamineShMem::setTitle(std::string name, UInt_t slot) {}
+void CTestXamineShMem::setInfo(CSpectrum& rSpectrum, std::string name) {}
+void CTestXamineShMem::setInfo(std::string  name, UInt_t slot) {}
+
+
+std::vector<CGateContainer> CTestXamineShMem::getAssociatedGates(const std::string& spectrumName,
+                                           CHistogrammer& rSorter) {}
+
+void CTestXamineShMem::addGate (CXamineGate& rGate) {}
+void CTestXamineShMem::removeGate(CSpectrum& rSpectrum, CGateContainer& rGate)  {}
+void CTestXamineShMem::removeGate (UInt_t nSpectrum, UInt_t nId, GateType_t eType)  {}
+CXamineGates* CTestXamineShMem::GetGates (UInt_t nSpectrum)  {}
+
+void CTestXamineShMem::setUnderflows(unsigned slot, unsigned x, unsigned y) {}
+void CTestXamineShMem::setOverflows(unsigned slot, unsigned x, unsigned y) {}
+void CTestXamineShMem::clearStatistics(unsigned slot) {}
--- main/Core/TestXamineShMem.h
+++ main/Core/TestXamineShMem.h
@@ -1,10 +1,61 @@
 #ifndef TESTXAMINESHMEM_H
 #define TESTXAMINESHMEM_H
 
+#include "XamineSharedMemory.h"
+
 class CTestXamineShMem : public CXamineSharedMemory
 {
+private:
+
+
 public:
     CTestXamineShMem();
+
+    virtual void setManaged(bool value);
+
+    virtual bool isManaged() const;
+
+    virtual void attach();
+    virtual void detach();
+
+    virtual size_t getSize() const;
+
+    virtual UInt_t addSpectrum(CSpectrum& , CHistogrammer& );
+    virtual void removeSpectrum(UInt_t slot, CSpectrum& );
+
+    virtual DisplayBindings getDisplayBindings() const;
+
+    virtual CXamineSpectrum& operator[](UInt_t n);
+    virtual DisplayBindingsIterator displayBindingsBegin();
+    virtual DisplayBindingsIterator displayBindingsEnd();
+
+    virtual UInt_t displayBindingsSize() const;
+    virtual Int_t findDisplayBinding(std::string name);
+    virtual Int_t findDisplayBinding(CSpectrum& rSpectrum);
+
+    virtual void addFit(CSpectrumFit& fit);
+    virtual void deleteFit(CSpectrumFit& fit);
+
+    virtual std::string createTitle(CSpectrum& rSpectrum,
+                            UInt_t maxLength,
+                            CHistogrammer &rSorter);
+    virtual UInt_t getTitleSize()  const;
+    virtual void setTitle(std::string name, UInt_t slot);
+    virtual void setInfo(CSpectrum& rSpectrum, std::string name);
+    virtual void setInfo(std::string  name, UInt_t slot);
+
+
+    virtual std::vector<CGateContainer> getAssociatedGates(const std::string& spectrumName,
+                                               CHistogrammer& rSorter);
+
+    virtual void addGate (CXamineGate& rGate);
+    virtual void removeGate(CSpectrum& rSpectrum, CGateContainer& rGate) ;
+    virtual void removeGate (UInt_t nSpectrum, UInt_t nId, GateType_t eType) ;
+    virtual CXamineGates* GetGates (UInt_t nSpectrum) ;
+
+    virtual void setUnderflows(unsigned slot, unsigned x, unsigned y);
+    virtual void setOverflows(unsigned slot, unsigned x, unsigned y);
+    virtual void clearStatistics(unsigned slot);
 };
 
 #endif // TESTXAMINESHMEM_H
