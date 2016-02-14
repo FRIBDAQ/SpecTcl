58ba825e348db6780849a6ca92d2440bba2421d4
Jeromy Tompkins <tompkins@nscl.msu.edu>
2016-02-14 13:51:02 -0500

    Adds empty CTestXamineShMem

---
+++ main/Core/TestXamineShMem.cpp
@@ -0,0 +1,5 @@
+#include "TestXamineShMem.h"
+
+CTestXamineShMem::CTestXamineShMem()
+{
+}
---
+++ main/Core/TestXamineShMem.h
@@ -0,0 +1,10 @@
+#ifndef TESTXAMINESHMEM_H
+#define TESTXAMINESHMEM_H
+
+class CTestXamineShMem : public CXamineSharedMemory
+{
+public:
+    CTestXamineShMem();
+};
+
+#endif // TESTXAMINESHMEM_H
