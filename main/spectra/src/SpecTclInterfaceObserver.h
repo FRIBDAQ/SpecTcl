e6d02bb701fc7e652dec688c960978fdc4f7bc2b
Jeromy Tompkins <tompkins@nscl.msu.edu>
2016-03-01 12:11:29 -0500

    Documents the SpecTclInterfaceObserver

--- main/spectra/src/SpecTclInterfaceObserver.h
+++ main/spectra/src/SpecTclInterfaceObserver.h
@@ -5,19 +5,30 @@
 
 namespace Viewer {
 
+/// \class SpecTclInterfaceObserver
+///
+/// There should be be a single instance of the SpecTclInterface that exists at
+/// any given time in the program. This instance can be swapped out though with a
+/// different instance. When that happens the SpecTclInterface is replaced,
+/// all of the dependent objects (HistogramView, ControlPanel, etc.) need to be passed
+/// the new SpecTclInterface so that they can set up their respective signals and slots.
+/// Derived classes of this interface can be registered to the MainWindow to be
+/// notified when a change happens.
 class SpecTclInterfaceObserver {
 public:
     virtual ~SpecTclInterfaceObserver() {}
     virtual void update(std::shared_ptr<SpecTclInterface> pInterface ) = 0;
 };
 
-
-
-///  A generic wrapper to implement the observer dependencies
+/// \class GenericSpecTclInterfaceObserver
+///
+/// A generic wrapper to implement the observer dependencies for the
+/// SpecTclInterface. This is simply an adaptor class that delegates to an object
+/// it maintains a reference to.
 template<class T>
 class GenericSpecTclInterfaceObserver : public SpecTclInterfaceObserver {
 private:
-    T* m_pObj;
+    T* m_pObj; ///< the object to delegate to
 
 public:
     GenericSpecTclInterfaceObserver(T& obj) : m_pObj(&obj) {}
@@ -32,6 +43,10 @@ public:
         return *this;
     }
 
+    /*! Pass the SpecTclInterface to the object
+     *
+     *  \param pInterface   the new instance of SpecTclInterface
+     */
     void update(std::shared_ptr<SpecTclInterface> pInterface) {
         m_pObj->setSpecTclInterface(pInterface);
     }
