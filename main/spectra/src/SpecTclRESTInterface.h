eeaa7ebf60feee69998d900f3ce9b58c8c7dae26
Jeromy Tompkins <tompkins@nscl.msu.edu>
2016-03-01 13:00:23 -0500

    Documents the SpecTclRESTInterface

--- main/spectra/src/SpecTclRESTInterface.h
+++ main/spectra/src/SpecTclRESTInterface.h
@@ -46,37 +46,146 @@ namespace Viewer
 class GSlice;
 class QRootCanvas;
 
-/*! \brief Implementation of the SpecTcl facade for the REST server plugin
+/*! \brief Implementation of the SpecTcl facade for the REST server.
  *
+ *  This provides a simple interface to the subsystem of objects that handle
+ *  interactions with the REST server.
  */
 class SpecTclRESTInterface : public SpecTclInterface
 {
   Q_OBJECT
 public:
+    /*!
+     * \brief Constructor
+     *
+     * Creates a new HistogramList, GateList, and request handlers.
+     */
     SpecTclRESTInterface();
     virtual ~SpecTclRESTInterface();
 
+    ///////////////////////////////////////////////////////////////////////////
+    // Gate operations
+    /*!
+     * \brief Send REST request to add a new 1D gate
+     *
+     * \param slice     the slice to add
+     */
     void addGate(const GSlice& slice);
+
+    /*!
+     * \brief Send REST request to modify a 1D gate
+     *
+     * \param slice     the slice to edit
+     */
     void editGate(const GSlice& slice);
+
+    /*!
+     * \brief Send REST request to "delete" a 1D gate
+     *
+     * \param slice     the slice to delete
+     */
     void deleteGate(const GSlice& slice);
 
+    /*!
+     * \brief Send REST request to add a new 2D gate
+     *
+     * \param gate  the gate to add
+     */
     void addGate(const GGate& slice);
+
+    /*!
+     * \brief Send REST request to edit a new 2D gate
+     *
+     * \param gate  the gate to edit
+     */
     void editGate(const GGate& slice);
+    /*!
+     * \brief Send REST request to delete a new 2D gate
+     *
+     * \param gate the gate to delete
+     */
     void deleteGate(const GGate& slice);
 
+    /*!
+     * \brief Send REST request to delete a generic gate
+     *
+     * \param name name of gate to delete
+     */
     void deleteGate(const QString& name);
 
+    /*!
+     * \brief enableGatePolling
+     *
+     * \param enable  turns on (true) or off (false) polling
+     */
     void enableGatePolling(bool enable);
+
+    /*!
+     * \brief Check whether gate polling is enabled
+     *
+     * \returns boolean
+     * \retval true - polling active
+     * \retval false - polling disabled
+     */
     bool gatePollingEnabled() const { return pollGates;}
 
+    /*!
+     * \brief Retrieve the list of gates in SpecTcl
+     *
+     * \return pointer to the gate list for this SpecTcl interface
+     */
+    GateList* getGateList() { return m_pGateList.get(); }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Histogram (Spectrum) operations
+
+    /*!
+     * \brief Enable Histogram Info Polling
+     *
+     * The caller can use this to turn polling on or off. If being turned off,
+     * the last active request will be allowed to complete, but will not be
+     * rescheduled if the user chooses not to.
+     *
+     * \param enable  whether to enable or disable polling
+     */
     void enableHistogramInfoPolling(bool enable);
+
+    /*!
+     * \brief Check whether hist info polling is enabled
+     *
+     * \return boolean
+     * \retval true - polling is active
+     * \retval false - polling is inactive
+     */
     bool histogramInfoPollingEnabled() const { return pollHistInfo;}
 
-    GateList* getGateList() { return m_pGateList.get(); }
+    /*!
+     * \brief Retrieve the histogram list
+     *
+     * \return pointer to histogram list for this interface
+     */
     HistogramList* getHistogramList() { return m_pHistList.get(); }
 
+    /*!
+     * \brief Request content updates for all histograms in a canvas
+     *
+     * \param pCanvas - the canvas containing the histogram
+     */
     void requestHistContentUpdate(QRootCanvas *pView);
+
+    /*!
+     * \brief Request content updates for all histograms in a pad
+     * \param pPad - the pad containing the histograms
+     */
     void requestHistContentUpdate(TPad *pPad);
+
+    /*!
+     * \brief Request content updates for a specific histogram
+     *
+     * This accesses histogram content via the REST interface.
+     *
+     * \param hName - name of histogram to update
+     */
     void requestHistContentUpdate(const QString& hName);
 
 public slots:
