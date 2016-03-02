e785d3a1a59140e4f3d6ffd914dec17673e40631
Jeromy Tompkins <tompkins@nscl.msu.edu>
2016-03-02 12:46:02 -0500

    Documents CanvasOps.h

--- main/spectra/src/CanvasOps.h
+++ main/spectra/src/CanvasOps.h
@@ -11,9 +11,28 @@ namespace Viewer {
 
 class QRootCanvas;
 
+/*!
+ * Functions to extract information about canvases
+ */
 namespace CanvasOps {
 
+/*!
+ * \brief extractAllHistNames
+ *
+ * \param rCanvas   the canvas to search
+ *
+ * This is recursive and can handle subpads.
+ *
+ * \return list of histogram names that are contained in canvas
+ */
 std::vector<QString> extractAllHistNames(QRootCanvas& rCanvas);
+
+/*!
+ * \brief extractAllHistNames
+ * \param rPad  the pad to search
+ *
+ * This is the same as the above method except for the argument type.
+ */
 std::vector<QString> extractAllHistNames(TPad& rPad);
 
 } // end CanvasOps namespace
