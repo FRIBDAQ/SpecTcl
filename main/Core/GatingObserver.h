a8200e88b21834ca3af73cb10ef747c6500f0b7e
Jeromy Tompkins <tompkins@nscl.msu.edu>
2016-03-03 14:34:47 -0500

    Documents GatingObserver

--- main/Core/GatingObserver.h
+++ main/Core/GatingObserver.h
@@ -5,13 +5,34 @@ class CGateContainer;
 class CSpectrum;
 class CHistogrammer;
 
+/*!
+ * \brief An observer for gating actions
+ *
+ * This is a purely virtual base class so it merely defines the interface
+ * for derived classes to implement. The observer should be able to handle
+ * when a gate is applied or removed from a spectrum.
+ */
 class CGatingObserver
 {
 public:
     virtual ~CGatingObserver() {};
 
+    /*!
+     * \brief Gate has been applied to the spectrum
+     * \param rGate     - the gate
+     * \param rSpectrum - the spectrum
+     * \param rSorter   - access to the dictionaries
+     */
     virtual void onApply(const CGateContainer& rGate, CSpectrum& rSpectrum,
                          CHistogrammer& rSorter) = 0;
+
+
+    /*!
+     * \brief Gate has been removed from the spectrum
+     * \param rGate     - the gate
+     * \param rSpectrum - the spectrum
+     * \param rSorter   - access to the dictionaries
+     */
     virtual void onRemove(const CGateContainer& rGate, CSpectrum& rSpectrum,
                           CHistogrammer& rSorter) = 0;
 
