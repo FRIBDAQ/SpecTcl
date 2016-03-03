cf7b8995d83f39a87d74053e9a1f666c5a0cb3fc
Jeromy Tompkins <tompkins@nscl.msu.edu>
2016-03-03 15:27:03 -0500

    Documents SpectraProcess class

--- main/Core/SpectraProcess.h
+++ main/Core/SpectraProcess.h
@@ -6,6 +6,11 @@
 namespace Spectra
 {
 
+/*!
+ * \brief The CSpectraProcess class
+ *
+ * Provides the logic for launching the spectra program.
+ */
 class CSpectraProcess : public Subprocess
 {
 private:
@@ -15,12 +20,34 @@ public:
     CSpectraProcess();
     virtual ~CSpectraProcess();
 
+    /*!
+     * \brief Fork and execute the program
+     */
     void exec();
+
+    /*!
+     * \brief Kill off the program if it is running
+     */
     void kill();
 
+    /*!
+     * \brief Retrieve the pid
+     * \return pid
+     * \retval 0    process is not running
+     * \retval >0   process is running
+     */
     int getPid() const;
     bool isRunning() const;
 
+    /*!
+     * \brief Generate the file path to the executable
+     *
+     * The default location that is used is prefix/bin/spectra.
+     * If the SPECTRA_EXECUTABLE_PATH environment variable is set,
+     * it overrides the default location.
+     *
+     * \return path to executable
+     */
     std::string generatePath() const;
 
 };
