b3ef0fe152bfa5dd19c580ff1d8da24e29d789c3
Jeromy Tompkins <tompkins@nscl.msu.edu>
2016-07-05 11:28:17 -0400

    ROOT no longer manages hists in win file test

--- main/spectra/test/WinFileWriterTest.cpp
+++ main/spectra/test/WinFileWriterTest.cpp
@@ -59,10 +59,6 @@
 
 using namespace std;
 
-// these are needed for the win file parser
-extern spec_shared* xamine_shared;
-extern spec_shared* spectra;
-
 Win::SpectrumQueryInterface gSpectrumInterface;
 
 
@@ -82,13 +78,13 @@ public:
 
     CPPUNIT_TEST_SUITE(WinFileWriterTest);
     CPPUNIT_TEST(canvasDivision_0);
-//    CPPUNIT_TEST(canvasDivision_1);
-//    CPPUNIT_TEST(canvasContainsHists_0);
-//    CPPUNIT_TEST(canvasContainsHists_1);
-//    CPPUNIT_TEST(canvasTitle_0);
-//    CPPUNIT_TEST(canvasTitle_1);
-//    CPPUNIT_TEST(histRendition_0);
-//    CPPUNIT_TEST(histRendition_1);
+    CPPUNIT_TEST(canvasDivision_1);
+    CPPUNIT_TEST(canvasContainsHists_0);
+    CPPUNIT_TEST(canvasContainsHists_1);
+    CPPUNIT_TEST(canvasTitle_0);
+    CPPUNIT_TEST(canvasTitle_1);
+    CPPUNIT_TEST(histRendition_0);
+    CPPUNIT_TEST(histRendition_1);
     CPPUNIT_TEST_SUITE_END();
 
 public:
@@ -133,11 +129,6 @@ public:
         WinFileWriter writer;
         writer.writeTab(workspace, ".test_win.win");
 
-        // we need the shared memory because otherwise this does not work. the
-        // win file parser expects the shared_memory to exist and be filled with
-        // the appropriate stuff.
-        setUpSharedMemory();
-
         m_pDatabase = new win_db;
         m_pDatabase->read(".test_win.win");
 
@@ -148,49 +139,49 @@ public:
         m_pDatabase = nullptr;
         std::remove(".test_win.win");
 
-        tearDownSharedMemory();
+//        tearDownSharedMemory();
 
     }
 
-    void setUpSharedMemory()
-    {
-        // I wanted to avoid doing this, but the win file parser
-        // is very tightly coupled to the shared memory.
-        Xamine_CreateSharedMemory(1024*1024, &m_pSharedMemory);
-        Xamine_ManageMemory();
-        Xamine_initspectra();
-
-        // create two spectra with appropriate info so that the parser
-        // doesn't bail when looking up spectrum names.
-        Xamine_Allocate1d(&m_xamine1DSlot,10, "asdf", 1);
-        Xamine_Allocate2d(&m_xamine2DSlot, 10, 10, "lkjh", 2);
-    }
-
-    void tearDownSharedMemory()
-    {
-        Xamine_FreeSpectrum(m_xamine1DSlot);
-        Xamine_FreeSpectrum(m_xamine2DSlot);
-        m_xamine1DSlot = -1;
-        m_xamine2DSlot = -1;
-
-        // Locate the id for our shared mem
-        char name[5];
-        key_t key = 0;
-
-        Xamine_GetMemoryName(name);
-        memcpy(reinterpret_cast<char*>(&key), name, sizeof(name) );
-        int id = shmget(key, 1024*1024, 0);
-
-        // detach - reverse the effects of Xamine_CreateSharedMemory
-        Xamine_DetachSharedMemory();
-        // detach - reverse the effects of Xamine_initspectra
-        int stat = shmdt(spectra);
-
-        // wait on the daemon that is trying to clean up the shared memory
-        int childStatus = 0;
-        wait( &childStatus );
-        shmctl(id, IPC_RMID, 0);
-    }
+//    void setUpSharedMemory()
+//    {
+//        // I wanted to avoid doing this, but the win file parser
+//        // is very tightly coupled to the shared memory.
+//        Xamine_CreateSharedMemory(1024*1024, &m_pSharedMemory);
+//        Xamine_ManageMemory();
+//        Xamine_initspectra();
+//
+//        // create two spectra with appropriate info so that the parser
+//        // doesn't bail when looking up spectrum names.
+//        Xamine_Allocate1d(&m_xamine1DSlot,10, "asdf", 1);
+//        Xamine_Allocate2d(&m_xamine2DSlot, 10, 10, "lkjh", 2);
+//    }
+//
+//    void tearDownSharedMemory()
+//    {
+//        Xamine_FreeSpectrum(m_xamine1DSlot);
+//        Xamine_FreeSpectrum(m_xamine2DSlot);
+//        m_xamine1DSlot = -1;
+//        m_xamine2DSlot = -1;
+//
+//        // Locate the id for our shared mem
+//        char name[5];
+//        key_t key = 0;
+//
+//        Xamine_GetMemoryName(name);
+//        memcpy(reinterpret_cast<char*>(&key), name, sizeof(name) );
+//        int id = shmget(key, 1024*1024, 0);
+//
+//        // detach - reverse the effects of Xamine_CreateSharedMemory
+//        Xamine_DetachSharedMemory();
+//        // detach - reverse the effects of Xamine_initspectra
+//        int stat = shmdt(spectra);
+//
+//        // wait on the daemon that is trying to clean up the shared memory
+//        int childStatus = 0;
+//        wait( &childStatus );
+//        shmctl(id, IPC_RMID, 0);
+//    }
 
 
     std::unique_ptr<Viewer::HistogramBundle> createHist1D(const string& name) {
@@ -198,9 +189,13 @@ public:
         using namespace Viewer;
 
         std::unique_ptr<TH1> pHist(new TH1F(name.c_str(),"", 10, 0, 10));
+
+				// This is very important because it prevents ROOT from deleting the histogram itself
+				pHist->SetDirectory(0);
+
         std::unique_ptr<QMutex> pMutex(new QMutex);
-        std::unique_ptr<HistogramBundle> pBundle(new HistogramBundle(move(pMutex), move(pHist),
-        {name, 1, {"param"}, {{0, 10, 10}}, SpJs::Long}));
+				SpJs::HistInfo hInfo = {name, 1, {"param"}, {{0, 10, 10}}, SpJs::Long};
+        std::unique_ptr<HistogramBundle> pBundle(new HistogramBundle(move(pMutex), move(pHist), hInfo));
 
         return move(pBundle);
     }
@@ -210,6 +205,10 @@ public:
         using namespace Viewer;
 
         std::unique_ptr<TH1> pHist(new TH2F(name.c_str(),"", 10, 0, 10, 10, 0, 10));
+
+				// This is very important because it prevents ROOT from deleting the histogram itself
+				pHist->SetDirectory(0);
+
         std::unique_ptr<QMutex> pMutex(new QMutex);
         std::unique_ptr<HistogramBundle> pBundle(new HistogramBundle(move(pMutex), move(pHist),
         {name, 2, {"paramx", "paramy"}, {{0, 10, 10}, {0, 10, 10}}, SpJs::Long}));
