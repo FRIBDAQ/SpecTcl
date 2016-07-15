39d582673d6c9b5a93dac7c3c4ff3f730f013041
Jeromy Tompkins <tompkins@nscl.msu.edu>
2016-07-15 13:37:19 -0400

    Documents the code for subscribable hist

--- main/spectra/src/TestH1Subscriber.h
+++ main/spectra/src/TestH1Subscriber.h
@@ -6,15 +6,32 @@
 namespace Viewer
 {
 
+/*!
+ * \brief A testing implement of H1Subscriber
+ *
+ */
 class TestH1Subscriber : public H1Subscriber
 {
 
 private:
-    bool m_notified;
+    bool m_notified;    ///< flag for whether it has been notified
 
 public:
+    /*!
+     * \brief Constructor
+     *
+     * By default, the construction leaves this in a state that isNotified()
+     * returns false.
+     */
     TestH1Subscriber();
+
+    /*! \return whether it has been notified
+     */
     bool isNotified() const { return m_notified; }
+
+    /*!
+     * \brief sets m_notified to true
+     */
     void notify();
 };
 
