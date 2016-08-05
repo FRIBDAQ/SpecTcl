32aaf95e07ad6291c3ba1754ed5a3d1651f3693f
Jeromy Tompkins <tompkins@nscl.msu.edu>
2016-08-05 11:20:42 -0400

    Adds functions to retrieve draw options
    - CanvasOps gets the getDrawOption, setDrawOption, and findOptionLink
      methods.
    - New methods are documented
    - Tests are in place and code passes the tests

--- main/spectra/test/CanvasOpsTest.cpp
+++ main/spectra/test/CanvasOpsTest.cpp
@@ -35,11 +35,13 @@
 #include "TH1.h"
 #include "TH2.h"
 #include "TCanvas.h"
+#include "TList.h"
 
 #include <QString>
 
 #include <iostream>
 #include <algorithm>
+#include <stdexcept>
 
 using namespace std;
 
@@ -52,6 +54,14 @@ class CanvasOpsTest : public CppUnit::TestFixture
     CPPUNIT_TEST_SUITE( CanvasOpsTest );
     CPPUNIT_TEST( extractAllHistNames_0 );
     CPPUNIT_TEST( extractAllHistNames_1 );
+    CPPUNIT_TEST( getDrawOption_0 );
+    CPPUNIT_TEST( getDrawOption_1 );
+    CPPUNIT_TEST( getDrawOption_2 );
+    CPPUNIT_TEST( getDrawOption_3 );
+    CPPUNIT_TEST( setDrawOption_0 );
+    CPPUNIT_TEST( setDrawOption_1 );
+    CPPUNIT_TEST( setDrawOption_2 );
+    CPPUNIT_TEST( setDrawOption_3 );
     CPPUNIT_TEST_SUITE_END();
 
   public:
@@ -113,6 +123,88 @@ class CanvasOpsTest : public CppUnit::TestFixture
         EQMSG("Recursive search finds 2D histograms",
               QString("h2d"), names[1]);
 
+    }
+
+
+    void getDrawOption_0() {
+        QRootCanvas canvas(nullptr);
+        TH1F h1f("h1f", "", 10, 0, 10);
+        h1f.Draw("hist0 c");
+
+        EQMSG("We should be able to retrieve the correct draw option",
+              QString("hist0 c"), CanvasOps::getDrawOption(canvas.getCanvas(), &h1f));
+    }
+
+    void getDrawOption_1() {
+        TH1F h1f("h1f", "", 10, 0, 10);
+
+        CPPUNIT_ASSERT_THROW_MESSAGE(
+                    "An exception must be thrown if the pad arg is a nullptr",
+                    CanvasOps::getDrawOption(nullptr, &h1f),
+                    std::runtime_error);
+    }
+
+    void getDrawOption_2() {
+        QRootCanvas canvas(nullptr);
+
+        CPPUNIT_ASSERT_THROW_MESSAGE(
+                    "An exception must be thrown if the histogram arg is a nullptr",
+                    CanvasOps::getDrawOption(canvas.getCanvas(), nullptr),
+                    std::runtime_error);
+    }
+
+    void getDrawOption_3() {
+        QRootCanvas canvas(nullptr);
+        TH1F h1f("h1f", "", 10, 0, 10);
+
+        CPPUNIT_ASSERT_THROW_MESSAGE(
+                    "An exception must be thrown if the pad does not contain the hist",
+                    CanvasOps::getDrawOption(canvas.getCanvas(), &h1f),
+                    std::runtime_error);
+    }
+
+
+    void setDrawOption_0() {
+        QRootCanvas canvas(nullptr);
+        TH1F h1f("h1f", "", 10, 0, 10);
+        h1f.Draw();
+
+        TObjLink* pLink = CanvasOps::findOptionLink(*canvas.getCanvas(), h1f);
+
+        CanvasOps::setDrawOption(canvas.getCanvas(), &h1f, "lego");
+
+        EQMSG("We should be able to retrieve the correct draw option",
+              QString("lego"), QString::fromUtf8(pLink->GetOption()));
+
+    }
+
+
+    void setDrawOption_1() {
+        TH1F h1f("h1f", "", 10, 0, 10);
+
+        CPPUNIT_ASSERT_THROW_MESSAGE(
+                    "An exception must be thrown if the pad arg is a nullptr",
+                    CanvasOps::setDrawOption(nullptr, &h1f, ""),
+                    std::runtime_error);
+    }
+
+    void setDrawOption_2() {
+        QRootCanvas canvas(nullptr);
+
+        CPPUNIT_ASSERT_THROW_MESSAGE(
+                    "An exception must be thrown if the histogram arg is a nullptr",
+                    CanvasOps::setDrawOption(canvas.getCanvas(), nullptr, ""),
+                    std::runtime_error);
+    }
+
+    void setDrawOption_3() {
+        QRootCanvas canvas(nullptr);
+        TH1F h1f("h1f", "", 10, 0, 10);
+
+        CPPUNIT_ASSERT_THROW_MESSAGE(
+                    "An exception must be thrown if the pad does not contain the hist",
+                    CanvasOps::setDrawOption(canvas.getCanvas(), &h1f, ""),
+                    std::runtime_error);
     }
 
 };
