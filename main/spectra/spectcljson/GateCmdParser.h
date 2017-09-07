54f53c7f157d1d2048e74d8abda28303630fe8ad
Ron Fox <fox@nscl.msu.edu>
2017-09-07 16:16:55 -0400

    Remove True/False gate parses

--- main/spectra/spectcljson/GateCmdParser.h
+++ main/spectra/spectcljson/GateCmdParser.h
@@ -23,8 +23,7 @@ namespace SpJs
       std::unique_ptr<GateInfo> parseSlice(const Json::Value& value);
       std::unique_ptr<GateInfo> parseBand(const Json::Value& value);
       std::unique_ptr<GateInfo> parseContour(const Json::Value& value);
-      std::unique_ptr<GateInfo> parseFalse(const Json::Value& value);
-      std::unique_ptr<GateInfo> parseTrue(const Json::Value& value);
+
   };
 
 } // end of namespace
