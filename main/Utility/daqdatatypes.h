a51b96a5679eef0df2396e5458a2e91d47672b62
Ron Fox <fox@nscl.msu.edu>
2018-04-05 09:48:56 -0400

    Fix double defs for good -- accounting for them in libtclplus as well.

--- main/Utility/daqdatatypes.h
+++ main/Utility/daqdatatypes.h
@@ -39,7 +39,43 @@
 #ifndef DAQDATATYPES_H
 #define DAQDATATYPES_H
 #include <sys/types.h>
-#include <daqtypes.h>
+
+#if __cplusplus
+#if __cplusplus > 199711L // i.e. C++98
+// stdint.h is deprecated as of C++11 in favor of <cstdint>
+// The difference is that the types are brought into the std namespace.
+// We can mimic the old behavior by including cstdint and then bringing
+// them into the global scope with the using operator. This is a kludge 
+// but is the best solution for the moment.
+#include <cstdint>
+using std::int8_t;
+using std::uint8_t;
+using std::int16_t;
+using std::uint16_t;
+using std::int32_t;
+using std::uint32_t;
+using std::int64_t;
+using std::uint64_t;
+#else
+// When compiled with a C++ compiler that is not C++11 compatible
+// cstdint does not exist. Furthermore, stdint.h only includes the 
+// limit macros if __STDC_LIMIT_MACROS variables is defined. 
+#define __STDC_LIMIT_MACROS
+#include <stdint.h>
+#endif /* __cplusplus */
+#else
+// macros are part of the c standard
+# include <stdint.h>
+#endif /* C++ or C */
+
+
+typedef    int8_t      INT8;
+typedef    uint8_t     UINT8;
+typedef    int16_t     INT16;
+typedef    uint16_t    UINT16;
+typedef    int32_t     INT32;
+typedef    uint32_t    UINT32;
+
 
 
 typedef uint32_t ULong_t;
