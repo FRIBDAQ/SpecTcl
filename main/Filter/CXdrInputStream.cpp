6ef4ace6ec595efc9279291ec3a31d2a3583c4e7
Ron Fox <fox@nscl.msu.edu>
2018-04-05 15:15:09 -0400

    Fix initialization problem in operator>>(std::string&).

--- main/Filter/CXdrInputStream.cpp
+++ main/Filter/CXdrInputStream.cpp
@@ -312,6 +312,7 @@ CXdrInputStream&
 CXdrInputStream::operator>>(string& s) 
 {
   char* strbuf =  new char[m_nBuffersize];
+  strbuf[0] = 0;		// In case >> doesn't change buffer.
   (*this) >> strbuf;
   s = strbuf;
   delete []strbuf;
