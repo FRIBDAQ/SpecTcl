45d693848c0024f525b391c9b211c70494d7f211
Ron Fox <fox@nscl.msu.edu>
2018-08-14 08:55:38 -0400

    Completed testing.

--- main/ddas/fileunpackertests.cpp
+++ main/ddas/fileunpackertests.cpp
@@ -6,6 +6,9 @@
 
 #include <TreeParameter.h>
 #include <Event.h>
+#include <Histogrammer.h>
+#include <Globals.h>
+
 #include "DDASHit.h"
 #include <stdio.h>
 #include <unistd.h>
@@ -23,23 +26,60 @@ class fileupktests : public CppUnit::TestFixture {
   CPPUNIT_TEST(oneCrateOneModule);
   CPPUNIT_TEST(oneCrateTwoModules);
   CPPUNIT_TEST(twoCrates);
+  
+  CPPUNIT_TEST(badParse);
+  CPPUNIT_TEST(duplicateAddress);
+  
+  CPPUNIT_TEST(singleGoodHit);
+  CPPUNIT_TEST(multiGoodHits);
+  
+  CPPUNIT_TEST(unmappedHit);
   CPPUNIT_TEST_SUITE_END();
 
 
 private:
     std::string m_fileName;
+    CHistogrammer* m_pHistogrammer;
+    CEvent*     m_pEvent;
 public:
   void setUp() {
     m_fileName = "";
+    
+    m_pHistogrammer = new CHistogrammer;
+    gpEventSink     = m_pHistogrammer;
+    
+    m_pEvent = new CEvent;
+    CTreeParameter::setEvent(*m_pEvent);
+    
   }
   void tearDown() {
     if(!m_fileName.empty()) unlink(m_fileName.c_str());
+    delete m_pEvent;
+    delete m_pHistogrammer;
   }
 protected:
   void construction();
+  
+  // Good config files.
+  
   void oneCrateOneModule();
   void oneCrateTwoModules();
   void twoCrates();
+  
+  // Bad config files.
+  
+  void badParse();
+  void duplicateAddress();
+  
+  // Good unpacks.
+  
+  void singleGoodHit();
+  void multiGoodHits();
+  
+  // Failed unpacks.
+  
+  void unmappedHit();
+  
 };
 
 
@@ -63,6 +103,44 @@ createConfigFile(const std::vector<std::string>& contents)
     }
     
     return fileName;
+}
+
+/**
+ * makeHit
+ *    Make a (minimal) hit:
+ *
+ *  @param crate - crate of the hit.
+ *  @param slot  - Slot of the hit
+ *  @param channel - Channel of hit.
+ *  @param energy  - Energy of the hit
+ *  @return DAQ::DDAS::DDASHit - the resulting hit.l
+ */
+static DAQ::DDAS::DDASHit
+makeHit(unsigned crate, unsigned slot, unsigned channel, unsigned energy)
+{
+    DAQ::DDAS::DDASHit hit;
+    hit.setCrate(crate);
+    hit.setSlot(slot);
+    hit.setChannel(channel);
+    hit.setEnergy(energy);
+    
+
+    return hit;    
+}
+/**
+ * verifyParamerter
+ *    Verify that a parameter has a value and that it is specific.
+ *
+ * @param name - name of the parameter.
+ * @param value - required value of the parameter.
+ */
+static void verifyParameter(const char* name, double value)
+{
+    CTreeParameter param(name);
+    param.Bind();
+    ASSERT(param.isValid());
+    double pvalue = param;
+    EQ(value, pvalue);
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(fileupktests);
@@ -142,4 +220,116 @@ void fileupktests::twoCrates()
     
     EQ(std::string("crate1.slot2.channel0"), map[1][2][0]->getName());
     EQ(std::string("crate2.slot2.channel0"), map[2][2][0]->getName());
+}
+
+void fileupktests::badParse()
+{
+        std::vector<std::string> configFile = {
+        "0 2 0 12 slot2.channel0",
+        "larry curly moe shemp george"           // Ilegal.
+        };
+        m_fileName = createConfigFile(configFile);
+        
+        CPPUNIT_ASSERT_THROW(
+            CFileDrivenParameterMapper mapper(m_fileName.c_str()),
+            std::string
+        );
+}
+
+void fileupktests::duplicateAddress()
+{
+    std::vector<std::string> configFile = {
+        "0 2 0 12 slot2.channel0",
+        "0 2 0 12 slot2.channel0"
+    };
+    m_fileName = createConfigFile(configFile);
+    
+    CPPUNIT_ASSERT_THROW(
+        CFileDrivenParameterMapper mapper(m_fileName.c_str()),
+        std::string
+    );
+}
+
+
+void fileupktests::singleGoodHit()
+{
+    std::vector<std::string> configFile = {
+        "0 2 0 12 slot2.channel0",
+        "0 2 1 12 slot2.channel1",
+        "0 3 0 14 slot3.channel0",
+        "1 2 0 12 crate1.slot2.channel0",
+        "2 2 0 12 crate2.slot2.channel0"
+    };
+    
+    m_fileName = createConfigFile(configFile);
+    CFileDrivenParameterMapper mapper(m_fileName.c_str());
+    CTreeParameter::BindParameters();
+    
+    // Create a vector of single hits:
+    
+    std::vector<DAQ::DDAS::DDASHit> hits;
+    hits.push_back(makeHit(0,2,1,1234));
+    
+    // Unpack the data and check that it wound up in the parameter named
+    // "slot2.channel1"
+    
+    mapper.mapToParameters(hits, *m_pEvent);
+    verifyParameter("slot2.channel1", 1234);
+}
+
+
+void fileupktests::multiGoodHits()
+{
+    std::vector<std::string> configFile = {
+        "0 2 0 12 slot2.channel0",
+        "0 2 1 12 slot2.channel1",
+        "0 3 0 14 slot3.channel0",
+        "1 2 0 12 crate1.slot2.channel0",
+        "2 2 0 12 crate2.slot2.channel1"
+    };
+    
+    m_fileName = createConfigFile(configFile);
+    CFileDrivenParameterMapper mapper(m_fileName.c_str());
+    CTreeParameter::BindParameters();
+ 
+    std::vector<DAQ::DDAS::DDASHit> hits;
+    hits.push_back(makeHit(0, 2, 0, 100));
+    hits.push_back(makeHit(0, 2, 1, 200));
+    hits.push_back(makeHit(0, 3, 0, 300));
+    hits.push_back(makeHit(1, 2, 0, 400));
+    hits.push_back(makeHit(2, 2, 0, 500));
+    
+    mapper.mapToParameters(hits, *m_pEvent);
+    verifyParameter("slot2.channel0", 100);
+    verifyParameter("slot2.channel1", 200);
+    verifyParameter("slot3.channel0", 300);
+    verifyParameter("crate1.slot2.channel0", 400);
+    verifyParameter("crate2.slot2.channel1", 500);
+}
+
+void fileupktests::unmappedHit()
+{
+    std::vector<std::string> configFile = {
+        "0 2 0 12 slot2.channel0",
+        "0 2 1 12 slot2.channel1",
+        "0 3 0 14 slot3.channel0",
+        "1 2 0 12 crate1.slot2.channel0",
+        "2 2 0 12 crate2.slot2.channel1"
+    };
+    
+    m_fileName = createConfigFile(configFile);
+    CFileDrivenParameterMapper mapper(m_fileName.c_str());
+    CTreeParameter::BindParameters();
+ 
+    std::vector<DAQ::DDAS::DDASHit> hits;
+    hits.push_back(makeHit(0, 2, 0, 100));
+    hits.push_back(makeHit(0, 2, 1, 200));
+    hits.push_back(makeHit(0, 3, 0, 300));
+    hits.push_back(makeHit(1, 2, 0, 400));
+    hits.push_back(makeHit(3, 2, 0, 500));    // No mapping for this hit.
+    
+    CPPUNIT_ASSERT_THROW(
+        mapper.mapToParameters(hits, *m_pEvent),
+        std::string
+    );
 }
