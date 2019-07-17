36f9de3d2ed744fd91993e35b51812c5022c8e9b
Ron Fox <fox@nscl.msu.edu>
2019-07-17 12:59:13 -0400

    * Fix python build problems. * First hacks at SpecTcl command package.

---
+++ main/packaged/Spectclpackage.cpp
@@ -0,0 +1,102 @@
+/*
+    This software is Copyright by the Board of Trustees of Michigan
+    State University (c) Copyright 2017.
+
+    You may use this software under the terms of the GNU public license
+    (GPL).  The terms of this license are described at:
+
+     http://www.gnu.org/licenses/gpl.txt
+
+     Authors:
+             Ron Fox
+             Giordano Cerriza
+	     NSCL
+	     Michigan State University
+	     East Lansing, MI 48824-1321
+*/
+
+/** @file:  Spectclpackage.cpp
+ *  @brief: Contains package initialization code for SpecTcl as a loadable
+ *          package.
+ */
+#include <tcl.h>
+#include <SpecTcl.h>
+
+#include <Globals.h>
+
+// Commands we're going to import
+
+#include <ParameterPackage.h>
+#include <SpectrumPackage.h>
+#include <GatePackage.h>
+#include <FilterCommand.h>
+#include <CSpectrumStatsCommand.h>
+#include <CTreeParameterCommand.h>
+#include <CTreeVariableCommand.h>
+#include <CTreeParameter.h>
+#include <CTreeVariable.h>
+#include <CFoldCommand.h>
+#include <CFitCommand.h>
+#include <CProjectionCommand.h>
+#include <IntegrateCommand.h>
+#include <VersionCommand.h>
+#include <SContentsCommand.h>
+#include <CPipelineCommand.h>
+#include <CUnpackEvbCommand.h>
+#include <ProductionXamineShMem.h>
+
+#include <DisplayInterface.h>
+#include <TCLAnalyzer.h>
+#include <TCLHistogrammer.h>
+#include <TCLAnalyzer.h>
+
+
+#include <tcl.h>
+
+
+static const char* kpInstalledBase = INSTALLED_IN; // Preprocessor def.
+void* gpApplication(nullptr);
+
+
+extern "C" {
+    int Spectcl_Init(Tcl_Interp* pRawInterp)
+    {
+        Tcl_Init(pRawInterp);
+        CTCLInterpreter* pInterp = new CTCLInterpreter(pRawInterp);
+        
+        // Create enough of the SpecTcl scaffolding that none of the
+        // commands dies.
+        
+        gpInterpreter = pInterp;
+        gpEventSink = new CTCLHistogrammer(pInterp);
+        gpEventSinkPipeline = new CEventSinkPipeline;
+        gpEventSinkPipeline->AddEventSink(*gpEventSink, "::Histogrammer");
+        gpDisplayInterface = new CDisplayInterface;
+        gpAnalyzer = new CTclAnalyzer(*pInterp, 100, 1);
+        
+        // Add the SpecTcl Commands
+        
+        (new CParameterPackage(pInterp, (CTCLHistogrammer*)gpEventSink))->Register();
+
+        (new CSpectrumPackage(
+            pInterp, (CHistogrammer*)gpEventSink,gpDisplayInterface)
+        )->Register();
+        (new CGatePackage(pInterp, (CHistogrammer*)gpEventSink))->Register();
+
+      
+        new CTreeParameterCommand;
+        new CTreeVariableCommand;
+        CTreeVariable::BindVariables(*pInterp);      
+        new CFoldCommand(pInterp);
+        new CProjectionCommand(*pInterp);
+        new CFitCommand(*pInterp);
+        new CIntegrateCommand(*pInterp);        
+        new CVersionCommand(*pInterp);
+        new CSContentsCommand(*pInterp);
+        new CSpectrumStatsCommand(*pInterp);      
+        new CPipelineCommand(*pInterp);
+        new CUnpackEvbCommand(*pInterp);
+        
+        return TCL_OK;
+    }
+}
