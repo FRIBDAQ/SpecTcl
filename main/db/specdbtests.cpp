3b24eeaecf3ffa95669a9b96a630c75af9807fd5
Ron Fox <fox@nscl.msu.edu>
2020-04-01 11:30:13 +0000

    Add test template for SpecTcl::Database class.

---
+++ main/db/specdbtests.cpp
@@ -0,0 +1,51 @@
+/*
+    This software is Copyright by the Board of Trustees of Michigan
+    State University (c) Copyright 2017.
+
+    You may use this software under the terms of the GNU public license
+    (GPL).  The terms of this license are described at:
+
+     http://www.gnu.org/licenses/gpl.txt
+
+     Authors:
+             Ron Fox
+             Giordano Cerriza
+	     NSCL
+	     Michigan State University
+	     East Lansing, MI 48824-1321
+*/
+
+/** @file:  
+ *  @brief: 
+ */
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/Asserter.h>
+#include "Asserts.h"
+#include "SpecTclDatabase.h"
+#include "CSqlite.h"
+#include "CSqliteStatement.h"
+#include "CSqliteException.h"
+
+class specdbtest : public CppUnit::TestFixture {
+    CPPUNIT_TEST_SUITE(specdbtest);
+    CPPUNIT_TEST(test_1);
+    CPPUNIT_TEST_SUITE_END();
+    
+private:
+
+public:
+    void setUp() {
+        
+    }
+    void tearDown() {
+        
+    }
+protected:
+    void test_1();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(specdbtest);
+
+void specdbtest::test_1()
+{
+}
