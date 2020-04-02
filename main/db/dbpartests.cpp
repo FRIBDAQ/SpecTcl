f8c9aeaad6470080c50826ae57ae88083c2b5421
Ron Fox <fox@nscl.msu.edu>
2020-04-02 12:33:19 +0000

    Test template for the SpecTcl::DBParameter class.

---
+++ main/db/dbpartests.cpp
@@ -0,0 +1,54 @@
+/*
+    This software is Copyright by the Board of Trustees of Michigan
+    State University (c) Copyright 2017.
+
+    You may use this software under the terms of the GNU public license
+    (GPL).  The terms of this license are described at:
+
+     http://www.gnu.org/licenses/gpl.txt
+
+     Authors:
+             Ron Fox
+             Giordano Cerriza
+	     NSCL
+	     Michigan State University
+	     East Lansing, MI 48824-1321
+*/
+
+/** @file:  dbpartests.cpp
+ *  @brief: Test SpecTcl::DBParameter
+ */
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/Asserter.h>
+#include "Asserts.h"
+
+#include "CSqlite.h"
+#include "CSqliteStatement.h"
+#include "SpecTclDatabase.h"
+#include "SaveSet.h"
+#include "DBParameter.h"
+
+
+class aTestSuite : public CppUnit::TestFixture {
+    CPPUNIT_TEST_SUITE(aTestSuite);
+    CPPUNIT_TEST(test_1);
+    CPPUNIT_TEST_SUITE_END();
+    
+private:
+
+public:
+    void setUp() {
+        
+    }
+    void tearDown() {
+        
+    }
+protected:
+    void test_1();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(aTestSuite);
+
+void aTestSuite::test_1()
+{
+}
