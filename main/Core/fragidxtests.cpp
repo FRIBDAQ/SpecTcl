1ad80f0a7e6742cb78958f932cd1675165b79d8c
Ron Fox <fox@nscl.msu.edu>
2020-05-20 12:50:59 +0000

    daqdev/SpecTcl#378 Create test template for FragmentIndex and add it to the Makefile

---
+++ main/Core/fragidxtests.cpp
@@ -0,0 +1,47 @@
+/*
+    This software is Copyright by the Board of Trustees of Michigan
+    State University (c) Copyright 2017.
+
+    You may use this software under the terms of the GNU public license
+    (GPL).  The terms of this license are described at:
+
+     http://www.gnu.org/licenses/gpl.txt
+
+     Authors:
+             Ron Fox
+             Giordano Cerriza
+	     NSCL
+	     Michigan State University
+	     East Lansing, MI 48824-1321
+*/
+
+/** @file:  
+ *  @brief: 
+ */
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/Asserter.h>
+#include "Asserts.h"
+
+class fragidxTest : public CppUnit::TestFixture {
+    CPPUNIT_TEST_SUITE(fragidxTest);
+    CPPUNIT_TEST(test_1);
+    CPPUNIT_TEST_SUITE_END();
+    
+private:
+
+public:
+    void setUp() {
+        
+    }
+    void tearDown() {
+        
+    }
+protected:
+    void test_1();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(fragidxTest);
+
+void fragidxTest::test_1()
+{
+}
