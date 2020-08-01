b130036c15f6e4a55c280e3791488a06b474f7d8
Ron Fox <fox@nscl.msu.edu>
2020-08-01 14:25:23 +0000

    Added CAEN hit container class to the bulid.

---
+++ main/Core/CAENModuleHits.cpp
@@ -0,0 +1,89 @@
+/*
+    This software is Copyright by the Board of Trustees of Michigan
+    State University (c) Copyright 2017.
+
+    You may use this software under the terms of the GNU public license
+    (GPL).  The terms of this license are described at:
+
+     http://www.gnu.org/licenses/gpl.txt
+
+     Authors:
+             Ron Fox
+             Giordano Cerriza
+	     NSCL
+	     Michigan State University
+	     East Lansing, MI 48824-1321
+*/
+
+/** @file:  CAEHModuleHits.cpp
+ *  @brief: Collection of Hits from CAEN modules.
+ */
+#include "CAENModuleHits.h"
+
+/**
+ * constructor:
+ *    @param sid - id of the source (module number) from which
+ *                 these hits allegedly come.  This is intended for
+ *                 use in the stage when the hits we have are
+ *                 decoded into parameters.
+ */
+CAENModuleHits::CAENModuleHits(int sid) : m_nSourceId(sid)
+{
+    m_Hits.reserve(16);            // Current module size.
+}
+
+/**
+ * destructor
+ *    Currently empty.
+ */
+CAENModuleHits::~CAENModuleHits()
+{}
+
+/**
+ * clear
+ *    Clear the hits.  This makes us re-usable.  Note that
+ *    the vector's capacity is unchanged so we're not
+ *    flailing storage allocation (here).
+ */
+void
+CAENModuleHits::clear()
+{
+    m_Hits.clear();
+}
+/**
+ * addHit
+ *   Adds a pointer to an unspecialized hit to the container.
+ *   Note that in practice, all hits added to the container
+ *   will be of the same underlying type.
+ * @param pHit Pointer to the hit to add.
+ * @note that it's up to our client to manage storage.  The
+ *       parent retains ownership.
+ */
+void
+CAENModuleHits::addHit(CAENHit* pHit)
+{
+    m_Hits.push_back(pHit);
+}
+/**
+ * getHits
+ *   Returns the set of hits we currently have.  These
+ *   are an immutable reference, though theoretically the hits
+ *    are mutable.
+ * @return const std::vector<CAENHit*> 
+ */
+const std::vector<CAENHit*>
+CAENModuleHits::getHits() const
+{
+    return m_Hits;
+}
+/**
+ * getSourceId
+ *    Gets the id of the hit source.  This is effectively
+ *    a module id.
+ * @return int - source id.
+ */
+int
+CAENModuleHits::getSourceId() const
+{
+    return m_nSourceId;
+}
---
+++ main/Core/CAENModuleHits.h
@@ -0,0 +1,48 @@
+/*
+    This software is Copyright by the Board of Trustees of Michigan
+    State University (c) Copyright 2017.
+
+    You may use this software under the terms of the GNU public license
+    (GPL).  The terms of this license are described at:
+
+     http://www.gnu.org/licenses/gpl.txt
+
+     Authors:
+             Ron Fox
+             Giordano Cerriza
+	     NSCL
+	     Michigan State University
+	     East Lansing, MI 48824-1321
+*/
+
+/** @file:  CAENModuleHits.h
+ *  @brief: Provides for a module worth of CAENHits.
+ */
+#ifndef CAENMODULEHITS_H
+#define CAENMODULEHITS_H
+#include <vector>
+
+class CAENHit;
+/**
+ * @class
+ *    CAENModuleHits
+ *       Contains the hits associated with a module.
+ *       Note that these are stored as pointers to a set of undifferentiated
+ *       CAENHit base class items.  THe hits are assumed to have been
+ *       'newe'd in to existence.
+ */
+class CAENModuleHits {
+private:
+    int                       m_nSourceId;
+    std::vector<CAENHit*>     m_Hits;
+public:
+    CAENModuleHits(int sid);
+    virtual ~CAENModuleHits();
+    void clear();
+    void addHit(CAENHit* pHit);
+    const std::vector<CAENHit*> getHits() const;
+    int getSourceId() const;
+    
+};
+
+#endif
