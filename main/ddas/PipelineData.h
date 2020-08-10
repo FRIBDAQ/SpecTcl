db45820843b9fc77eec87c767a41072d1ad1ffe5
Cerizza, Giordano <cerizza@nscl.msu.edu>
2020-08-10 19:00:13 +0000

    Missing ddas/PipelineData.h

---
+++ main/ddas/PipelineData.h
@@ -0,0 +1,33 @@
+/*
+    This software is Copyright by the Board of Trustees of Michigan
+    State University (c) Copyright 2020.
+
+    You may use this software under the terms of the GNU public license
+    (GPL).  The terms of this license are described at:
+
+     http://www.gnu.org/licenses/gpl.txt
+
+     Authors:
+             Giordano Cerizza
+	     Ron Fox
+             NSCL
+             Michigan State University
+             East Lansing, MI 48824-1321
+*/
+#ifndef DAQ_DDAS_PIPELINEDATA_H
+#define DAQ_DDAS_PIPELINEDATA_H
+
+#include <memory>
+#include <vector>
+
+namespace DAQ {
+  namespace DDAS {
+    
+    class CPipelineData {
+    public:
+      virtual CPipelineData* clone() = 0;
+    };
+  }
+}
+
+#endif
