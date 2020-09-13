c157ad09140a248241ac965d33528d8d7b122d33
Ron Fox <fox@nscl.msu.edu>
2020-09-13 10:55:16 +0000

    CAENDPP PARSING TESTS
    Add Test template for CAENParser class/method tests.

---
+++ main/Core/parsertest.cpp
@@ -0,0 +1,52 @@
+/*
+    This software is Copyright by the Board of Trustees of Michigan
+    State University (c) Copyright 2017.
+
+    You may use this software under the terms of the GNU public license
+    (GPL).  The terms of this license are described at:
+
+     http://www.gnu.org/licenses/gpl.txt
+
+     Authors:
+             Ron Fox
+             Giordano Cerriza
+	     NSCL
+	     Michigan State University
+	     East Lansing, MI 48824-1321
+*/
+
+/** @file:  parsertest.cpp
+ *  @brief:  Test CAENParser class.
+ */
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/Asserter.h>
+#include "Asserts.h"
+#include "CAENParser.h"
+#include "CAENHit.h"
+#include "CAENModuleHits.h"
+
+
+
+class parsertest : public CppUnit::TestFixture {
+    CPPUNIT_TEST_SUITE(parsertest);
+    CPPUNIT_TEST(test_1);
+    CPPUNIT_TEST_SUITE_END();
+    
+private:
+
+public:
+    void setUp() {
+        
+    }
+    void tearDown() {
+        
+    }
+protected:
+    void test_1();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(parsertest);
+
+void parsertest::test_1()
+{
+}
