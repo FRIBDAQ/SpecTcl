4f65f255a50b813fe7295666e1913512644ba313
Ron Fox <fox@nscl.msu.edu>
2020-10-03 13:51:26 +0000

    CAEN Parsing
    added tests for CAENParser to for hit parsing object management.

--- main/Core/parsertest.cpp
+++ main/Core/parsertest.cpp
@@ -21,32 +21,102 @@
 #include <cppunit/extensions/HelperMacros.h>
 #include <cppunit/Asserter.h>
 #include "Asserts.h"
+#define private public
 #include "CAENParser.h"
+#undef private
 #include "CAENHit.h"
 #include "CAENModuleHits.h"
 
 
 
 class parsertest : public CppUnit::TestFixture {
-    CPPUNIT_TEST_SUITE(parsertest);
-    CPPUNIT_TEST(test_1);
-    CPPUNIT_TEST_SUITE_END();
     
 private:
-
+    CAENParser* m_pParser;
 public:
     void setUp() {
-        
+        m_pParser = new CAENParser;
     }
     void tearDown() {
-        
+        delete m_pParser;    
     }
+private:
+    CPPUNIT_TEST_SUITE(parsertest);
+    CPPUNIT_TEST(construct_1);
+    
+    CPPUNIT_TEST(add_1);
+    CPPUNIT_TEST(add_2);
+    CPPUNIT_TEST(add_3);
+    CPPUNIT_TEST_SUITE_END();
+
 protected:
-    void test_1();
+    void construct_1();
+    
+    void add_1();
+    void add_2();
+    void add_3();
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION(parsertest);
 
-void parsertest::test_1()
+void parsertest::construct_1()
 {
+    // without something to parse there are no hit modules:
+    
+    EQ(size_t(0), m_pParser->getHitModules().size());
+    EQ(size_t(0), m_pParser->m_modules.size());
+}
+void parsertest::add_1()
+{
+    // Add PSD and check structures:
+    
+    m_pParser->addModule(1, CAENHit::PSD);
+    EQ(size_t(1), m_pParser->m_modules.size());
+    const CAENParser::ModuleInfo& info = m_pParser->m_modules[1];
+    
+    EQ(CAENHit::PSD, info.s_hitType);
+    EQ(0, info.s_multiplier);
+    EQ(1, info.s_module.getSourceId());
+    EQ(size_t(0), info.s_module.getHits().size());
+    
+}
+void parsertest::add_2()
+{
+    // ADD PHA and check structures:
+    
+    m_pParser->addModule(2, CAENHit::PHA);
+    
+    EQ(size_t(1), m_pParser->m_modules.size());
+    const CAENParser::ModuleInfo& info = m_pParser->m_modules[2];
+    
+    EQ(CAENHit::PHA, info.s_hitType);
+    EQ(0, info.s_multiplier);
+    EQ(2, info.s_module.getSourceId());
+    EQ(size_t(0), info.s_module.getHits().size());
+}
+void parsertest::add_3()
+{
+    // Can add more than one and they're kept straight:
+    
+    m_pParser->addModule(1, CAENHit::PSD);
+    m_pParser->addModule(2, CAENHit::PHA);
+    
+    EQ(size_t(2), m_pParser->m_modules.size());\
+    // Using blocks like this allows a re-use of the assertions
+    //  from add_1, add_3
+    
+    {
+        const CAENParser::ModuleInfo& info = m_pParser->m_modules[1];
+        EQ(CAENHit::PSD, info.s_hitType);
+        EQ(0, info.s_multiplier);
+        EQ(1, info.s_module.getSourceId());
+        EQ(size_t(0), info.s_module.getHits().size());
+    }
+    {
+        const CAENParser::ModuleInfo& info = m_pParser->m_modules[2];
+        EQ(CAENHit::PHA, info.s_hitType);
+        EQ(0, info.s_multiplier);
+        EQ(2, info.s_module.getSourceId());
+        EQ(size_t(0), info.s_module.getHits().size());
+    }
 }
