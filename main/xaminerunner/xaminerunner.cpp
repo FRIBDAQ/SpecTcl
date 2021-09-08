b8fcb73086c8c2ee8e713c2a39edf73c473940ba
Ron Fox <fox@nscl.msu.edu>
2021-09-08 15:49:57 +0000

    daqdev/SpecTcl#407 Standalone Xamine attach to remote/local SpecTcls
    Initial hack at the main - parse the command line options and
    start the Tcl interpreter.

---
+++ main/xaminerunner/xaminerunner.cpp
@@ -0,0 +1,76 @@
+/*
+    This software is Copyright by the Board of Trustees of Michigan
+    State University (c) Copyright 2017.
+
+    You may use this software under the terms of the GNU public license
+    (GPL).  The terms of this license are described at:
+
+     http://www.gnu.org/licenses/gpl.txt
+
+     Authors:
+             Ron Fox
+             Giordano Cerriza
+	     NSCL
+	     Michigan State University
+	     East Lansing, MI 48824-1321
+*/
+
+/** @file:  xaminerunner.cpp
+ *  @brief: Main program for the Xamine Runner.
+ */
+
+/**
+ * Processes command line parameters,
+ * starts the intepreter.
+ * registers all those nice commands the main depends on.
+ * Finds and Sources in the script that executes the application.
+ */
+#include <tcl.h>
+
+#include "TCLInterpreter.h"
+#include "cmdline.h"
+
+/**
+ * Application initialization.
+ */
+
+static int AppInit(Tcl_Interp* pInterp)
+{
+    if (Tcl_Init(pInterp) != TCL_OK) {
+        return TCL_ERROR;
+    }
+    
+    // Wrap the interpreter in a CTCLInterpreter:
+    
+    CTCLInterpreter* pOInterp = new CTCLInterpreter(pInterp);
+    
+    // Set the initialization script for now just ~/.tclshrc -
+    // TODO: set it to the application script.
+    
+    Tcl_ObjSetVar2(
+        pInterp, Tcl_NewStringObj("tcl_rcFilename", -1), NULL,
+        Tcl_NewStringObj("~/.tclshrc", -1), TCL_GLOBAL_ONLY
+    );
+    
+    return TCL_OK;
+
+}
+
+//----------------------------------------------------------------------------
+// Entry point.
+// Put the parsed command junk here.
+
+static struct gengetopt_args_info parsed;
+
+/**
+ * main
+ *   Entry point.
+ */
+int main(int argc, char** argv)
+{
+    cmdline_parser(argc, argv, &parsed);
+    
+    // Start up Tcl, our App init will take over from here:
+    
+    Tcl_Main(argc, argv, AppInit);
+}
