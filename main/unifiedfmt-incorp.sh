3faa206ebb71956729aea3cc82e9025464214040
Genie Jhang <geniejhang@majimak.com>
2023-04-02 13:58:02 -0400

    Update unifiedfmt-incorp.sh

--- main/unifiedfmt-incorp.sh
+++ main/unifiedfmt-incorp.sh
@@ -4,7 +4,7 @@
 
 # Most recently used version 1.1-005
 
-REPOSITORY="https://github.com/FRIBDAQ/UnifiedFormat.git"
+REPOSITORY="git@github.com:FRIBDAQ/UnifiedFormat.git"
 TAG=$1
 
 rm -rf unifiedformat
