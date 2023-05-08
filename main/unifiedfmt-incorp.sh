3d3433b6ed5409d99785013d9258bb4fd94494a7
Ron Fox <fox@nscl.msu.edu>
2023-05-08 13:08:58 +0000

    *   Updated source of unified format lib and twiddled script appropriately. *   Document that 1.2 is the latest.

--- main/unifiedfmt-incorp.sh
+++ main/unifiedfmt-incorp.sh
@@ -2,12 +2,12 @@
 
 ##  Incorporate the unfied formatting library:
 
-# Most recently used version 1.1-005
+# Most recently used version 1.2
 
-REPOSITORY="git@github.com:FRIBDAQ/unifiedformat.git"
+REPOSITORY="https://github.com/FRIBDAQ/UnifiedFormat.git"
 TAG=$1
 
 rm -rf unifiedformat
-git clone $REPOSITORY
+git clone $REPOSITORY unifiedformat
 (cd unifiedformat; git checkout $TAG)
  
