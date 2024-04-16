946b0aecf2170fad38bb8bf1f5e594c655acb968
Ron Fox <fox@nscl.msu.edu>
2024-04-16 12:35:33 +0000

    Issue #116 Support an FRIB pipeline filter format.
    Verfied that tree variables get stored properly in the VariableItem (second) ring item in the file.

--- main/Core/fribfiltertests.cpp
+++ main/Core/fribfiltertests.cpp
@@ -46,6 +46,8 @@ const char* TempPlate = "fribvfilterXXXXXX";   // Temp file Template (get it?).
 class FribFilterTests : public CppUnit::TestFixture {
     CPPUNIT_TEST_SUITE(FribFilterTests);
     CPPUNIT_TEST(empty);
+    CPPUNIT_TEST(params_1);
+    CPPUNIT_TEST(vars_1);
     CPPUNIT_TEST_SUITE_END();
 
     // Data:
@@ -66,6 +68,8 @@ public:
 
 protected:
     void empty();
+    void params_1();
+    void vars_1();
 
 private:
     void makeTemp();
@@ -143,5 +147,100 @@ void FribFilterTests::empty() {
     std::unique_ptr<CRingItem> none(fact.getRingItem(m_fd));
     ASSERT(none.get() == 0);
 
+}
 
+void FribFilterTests::params_1() {
+    // Try some parameter definitions and see if they make it into the ring item:
+
+    std::vector<std::string> names = { "p1", "p2", "p3", "the_last"};
+    std::vector<UInt_t>     ids    = {   1,   3,    5,       2};
+
+    CFRIBFilterFormat filter;
+    filter.open(m_tempfile);
+    filter.DescribeEvent(names, ids);
+    filter.close();
+
+    if (lseek(m_fd, 0, SEEK_SET) < 0) {
+        throw std::runtime_error("FribFilterTests::params_2 failed to rewind test file");
+    }
+
+    v12::RingItemFactory fact;
+    // Get and verify the parameter description item.
+    std::unique_ptr<CRingItem> pitem(fact.getRingItem(m_fd));
+    ASSERT(pitem.get() != 0);
+    EQ(frib::analysis::PARAMETER_DEFINITIONS, pitem->type());
+    const frib::analysis::ParameterDefinitions* ppItem = 
+        reinterpret_cast<const frib::analysis::ParameterDefinitions*>(pitem->getItemPointer());
+    EQ(std::uint32_t(names.size()), ppItem->s_numParameters);
+
+    const frib::analysis::ParameterDefinition* pDef = ppItem ->s_parameters;
+    for (int i =0; i < names.size(); i++) {
+        EQ(std::uint32_t(ids[i]), pDef->s_parameterNumber);
+        std::string aname = pDef->s_parameterName;
+        EQ(names[i], aname);
+
+        // Next one:
+
+        const std::uint8_t* p = reinterpret_cast<const std::uint8_t*>(pDef);
+        p += sizeof(std::uint32_t) + aname.size() + 1;
+        pDef = reinterpret_cast<const frib::analysis::ParameterDefinition*>(p);
+    }
+
+
+}
+
+void FribFilterTests::vars_1() {
+    // Make some tree variables and ensure they are also stored properly in the second ring item:
+
+    CTreeVariable v1("pi", 3.1416, "radians");
+    CTreeVariable v2("pi_over_two", 3.1416/2.0, "radians");
+    CTreeVariable v3("forty-five", 45.0, "degrees");
+    CTreeVariable v4("inch", 2.54, "cm");
+
+    // These will come out of the dict in alpha order:
+
+    std::vector<std::string> names = {"forty-five", "inch", "pi", "pi-over_two"};
+    std::vector<double> values= {45.0, 2.54, 3.1416, 3.1416/2.0};
+    std::vector<std::string> units = {"degrees", "cm", "radians", "radians"};
+
+    //we already verified parameters and that should be orthogonal to this.
+
+    std::vector<std::string> pnames;
+    std::vector<UInt_t> pids;
+
+    CFRIBFilterFormat filter;
+    filter.open(m_tempfile);
+    filter.DescribeEvent(pnames, pids);
+    filter.close();
+
+    if (lseek(m_fd, 0, SEEK_SET) < 0) {
+        throw std::runtime_error("FribFilterTests::params_2 failed to rewind test file");
+    }
+
+    v12::RingItemFactory fact;
+    // Skip the parameter def.
+    std::unique_ptr<CRingItem> pitem(fact.getRingItem(m_fd));
+
+    // Get/verify the variable values:
+
+    std::unique_ptr<CRingItem> vitem(fact.getRingItem(m_fd));
+    ASSERT(vitem.get() != 0);
+    EQ(frib::analysis::VARIABLE_VALUES, vitem->type());
+    const frib::analysis::VariableItem* pvItem = 
+        reinterpret_cast<const frib::analysis::VariableItem*>(vitem->getItemPointer());
+    EQ(std::uint32_t(names.size()), pvItem->s_numVars);
+
+    const frib::analysis::Variable* pVar = pvItem->s_variables;
+    for (int i =0; i < names.size(); i++) {
+        std::string vunits = pVar->s_variableUnits;
+        std::string aname = pVar->s_variableName;
+
+        EQ(names[i], aname);
+        EQ(values[i], pVar->s_value);
+        EQ(units[i], vunits);
+
+        const std::uint8_t* p = reinterpret_cast<const std::uint8_t*>(pVar);
+        p += sizeof(double) + frib::analysis::MAX_UNITS_LENGTH + aname.size() + 1;
+        pVar = reinterpret_cast<const frib::analysis::Variable*>(p);
+    }
 }
