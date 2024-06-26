17492b018bd5d47573e5854451aac0c5f50cab78
Ron Fox <fox@frib.msu.edu>
2024-06-26 08:36:50 -0400

    Issue #141 - Make rootinterface package work in mpiSpecTcl
    *  Only define the RootExit command in the event sink rank.
    *  Wrap the rootexec command in an CMPITclCommand so it can be run in
    the event sink rank.
    *  Modify the exit handler so that I think it works properly.

--- main/RootInterface/rootPackage.cpp
+++ main/RootInterface/rootPackage.cpp
@@ -43,6 +43,10 @@
 #include <TRint.h>
 #include <TSystem.h>
 #include <iostream>
+#include <TclPump.h>
+#include <MPITclCommand.h>
+#include <Globals.h>
+#include "RootEventLoop.h"
 
 extern int SpecTclArgc;
 extern char** SpecTclArgv;
@@ -54,24 +58,28 @@ extern char** SpecTclArgv;
  *    Is a timer class that executes whenever the Tcl event loop is idle,
  *    asking Root to run any pending events it has.
  */
-class CRootEventLoop : public CTCLTimer
-{
-public:
-  CRootEventLoop(CTCLInterpreter* pInterp) :
-    CTCLTimer(pInterp, 100) {
-      Set();
-    }
-  ~CRootEventLoop() {}
+
+CRootEventLoop::CRootEventLoop(CTCLInterpreter* pInterp) :
+  CTCLTimer(pInterp, 100), m_exit(false) {
+    Set();
+  }
+CRootEventLoop::~CRootEventLoop() {}
   
-  virtual void operator()() {
+void 
+CRootEventLoop::operator()() {
     extern TSystem* gSystem;
+    if (m_exit) return;               // Also don't reschedule if asked to exit.
     if (!gSystem) return;             // Getting killed off.
     gSystem->ProcessEvents();         // Process root events.
     Set();                            // Reschedule
-  }
-};
+}
+void 
+CRootEventLoop::stop() {
+  m_exit = true;
+}
 
 
+CRootEventLoop* SpecTclRootEventLoop(0);
 
 static const char* version("1.0");
 extern "C" {
@@ -87,14 +95,29 @@ extern "C" {
         
   
         gApplication = new TRint("SpecTcl", &SpecTclArgc, SpecTclArgv );
-        new CRootEventLoop(pInterp);
-        
+
+        // In MPI SpecTcl, we run the event loop in the event sink pipeline.
+
+        if (!isMpiApp() || (myRank() == MPI_EVENT_SINK_RANK)) {
+          SpecTclRootEventLoop = new CRootEventLoop(pInterp);
+        }
         
         // Set up root interface commands:
         
-        new CRootExitCommand(*pInterp, "exit");    // Replace root exit command.
-        new TreeCommand(*pInterp, "roottree");
-        new CRootExec(*pInterp, "rootexec");
+        // Use the standard exit command everywhere but rank 2 if mpi
+        // This deserves explanation:
+        // exit will get done in the MPI_ROOT_RANK.  As it exits,
+        // the MPI exit handler will be called and it will send an exit command
+        // to the command pumps but not expect a response (wrapping this in a
+        // MPITclCOmmand will result in a hang on the responses).
+        // the event sink is where all of the root stuff runs and so that's the only 
+        // place we need a special exit command.
+        //
+        if (!isMpiApp() || (myRank() == MPI_EVENT_SINK_RANK)) {
+          new CRootExitCommand(*pInterp, "exit"); 
+        }
+        // new TreeCommand(*pInterp, "roottree");
+        new CMPITclCommand(*pInterp, "rootexec", new CRootExec(*pInterp, "rootexec"));
         
         std::cerr << " SpecTcl Root interface loaded: \n";
         std::cerr << "    - Root is a product of CERN (http://root.cern.ch)\n";
