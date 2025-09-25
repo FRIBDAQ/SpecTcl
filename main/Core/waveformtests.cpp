06e17e7740d6656e9200ac9cc63f0de0b37754d9
Ron Fox <fox@frib.msu.edu>
2025-09-25 08:18:07 -0400

    Issue #201 - Add support for waveforms to SpecTcl
    Test CWaveform construction and id assignment.

--- main/Core/waveformtests.cpp
+++ main/Core/waveformtests.cpp
@@ -22,12 +22,19 @@
 #include <cppunit/Asserter.h>
 #include "Asserts.h"
 #define private public
-#include <CWaveForm.h>
+#include "CWaveForm.h"
 #undef private
 
 class WaveformTests : public CppUnit::TestFixture {
 CPPUNIT_TEST_SUITE(WaveformTests);
+CPPUNIT_TEST(construct_1);
+CPPUNIT_TEST(construct_2);
 CPPUNIT_TEST_SUITE_END();
+
+
+protected:
+    void construct_1();
+    void construct_2();
 
 public:
     void setUp() {
@@ -37,3 +44,25 @@ public:
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION(WaveformTests);
+
+
+/// the tests
+
+void
+WaveformTests::construct_1() {
+    // Test simple construction of the first one:
+
+    CWaveform wf("Testing", 100);
+    EQ(std::string("Testing"), wf.getName());
+    EQ(UInt_t(0), wf.getNumber());
+    EQ(size_t(100), wf.size());
+}
+
+void
+WaveformTests::construct_2() {
+    // id auto increments:
+
+    CWaveform wf1("test1", 100);
+    CWaveform wf2("test2", 125);
+    EQ(UInt_t(1), wf2.getNumber());
+}
