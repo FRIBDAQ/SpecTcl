2d8e682ca9759920d5fd904a4a94643c672c6e65
Ron Fox <fox@frib.msu.edu>
2025-09-26 10:58:43 -0400

    Issue #201 - Add support for waveforms to SpecTcl
    Completely tested spectcl::serial::waveform create subcommand.

--- main/Core/wfcmdtests.cpp
+++ main/Core/wfcmdtests.cpp
@@ -43,6 +43,9 @@ class WFCmdTests : public CppUnit::TestFixture {
 private:
     void create_1();
     void create_2();
+    void create_3();
+    void create_4();
+    void create_5();
 // Test objects:
 private:
     CTCLInterpreter* m_pInterp;
@@ -90,6 +93,41 @@ WFCmdTests::create_2() {
     CPPUNIT_ASSERT_NO_THROW(
         m_pInterp->GlobalEval("spectcl::serial::waveform test 100")
     );
+
+    auto wf = SpecTcl::getInstance()->findWaveform("test");
+    ASSERT(wf);
+    EQ(size_t(100), wf->size());
+}
+
+void WFCmdTests::create_3() {
+    // not enough parameters.
+
+    CPPUNIT_ASSERT_THROW(
+        m_pInterp->GlobalEval("spectcL::serial::waveform create test"),
+        CTCLException
+    );
+}
+void WFCmdTests::create_4() {
+    // too many parameters.
+
+    CPPUNIT_ASSERT_THROW(
+        m_pInterp->GlobalEval("spectcL::serial::waveform create test 100 junk"),
+        CTCLException
+    );
+}
+void WFCmdTests::create_5() {
+    // duplicate should fail:
+
+    CPPUNIT_ASSERT_NO_THROW(
+        m_pInterp->GlobalEval("spectcl::serial::waveform create test 100")
+    );
+
+    CPPUNIT_ASSERT_THROW(
+        m_pInterp->GlobalEval("spctcl::serial::waveform create test 150"),    // already exists.
+        CTCLException
+    );
+
+    // THe old one is still there and has correct size.
 
     auto wf = SpecTcl::getInstance()->findWaveform("test");
     ASSERT(wf);
