d1289fef1301c62d66e956073929e894f38c4deb
Ron Fox <fox@frib.msu.edu>
2025-11-17 09:32:32 -0500

    Issue #212 Assocate fits with waveforms.
    Added the fits subcommand to the waveform command emulator.

--- main/restclient/SpecTclRestCommand.tcl
+++ main/restclient/SpecTclRestCommand.tcl
@@ -2086,7 +2086,7 @@ proc mirror {list {pattern *}} {
 #  It's a namespaxce ensemble.
 
 namespace eval waveform {
-    namespace export create metadata get resize   _defaultCreate
+    namespace export create metadata get resize   _defaultCreate fits getall
     namespace ensemble create
 
     proc _metadataToDict {metaList} {
@@ -2197,6 +2197,16 @@ namespace eval waveform {
     # @return none.
     proc resize {name samples} {
         return [$::SpecTclRestCommand::client waveformResize $name $samples]    
+    }
+
+    ##
+    # fits
+    #   List the fits that are associated with a waveform.
+    # 
+    # @param name - waveform name.
+    # @param pattern - optional pattern that restricts the set of fits returned.
+    proc fits {name {pattern *}} {
+        return [$::SpecTclRestCommand::client waveformListFits $name $pattern]
     }
         
 }
