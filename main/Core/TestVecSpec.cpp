95ea71c11e3dc23792bd806653fd7a202577a6bf
Ron Fox <fox@frib.msu.edu>
2026-04-01 09:15:32 -0400

    Issue #230 vector valued parameters.
    Tested set/get so that we know that we can check stuff.

--- main/Core/TestVecSpec.cpp
+++ main/Core/TestVecSpec.cpp
@@ -19,11 +19,13 @@ class Test1DVec : public CppUnit::TestFixture {
     CPPUNIT_TEST_SUITE(Test1DVec);
     CPPUNIT_TEST(construct_1);
     CPPUNIT_TEST(construct_2);
+    CPPUNIT_TEST(setget_1);
     CPPUNIT_TEST_SUITE_END();
 
 protected:
     void construct_1();
     void construct_2();
+    void setget_1();
 public:
     void setUp() {}
     void tearDown() {}
@@ -50,4 +52,16 @@ void Test1DVec::construct_2() {
 
     EQ(std::string("Testing"), spec.m_parameters.name());   // correct parameter.
     EQ(UInt_t(100), spec.m_nChannels);
+}
+// Before we can do interesting stuff, we need to be able to set/get chanels.
+
+void Test1DVec::setget_1() {
+    CTreeParameterVector p("Testing", 0.0, 100.0, "junk");
+    CSpectrum1DVecL spec("test", 0, p, 100, 0.0, 100.0);
+    UInt_t index=50;
+    spec.set(&index, 1234);
+
+    ULong_t value = spec[&index];
+
+    EQ(ULong_t(1234), value);
 }
