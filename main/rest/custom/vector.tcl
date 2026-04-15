0de859bd87976c3e0e0599ce37e7995782a37a29
Ron Fox <fox@frib.msu.edu>
2026-04-15 10:24:52 -0400

    Issue #230 vector valued parameters.
    Complete the vector operations in the ReST:
    /list, /setlow, /sethigh, /setbins, /setunits

--- main/rest/custom/vector.tcl
+++ main/rest/custom/vector.tcl
@@ -10,7 +10,7 @@ Direct_Url /spectcl/vector SpecTcl_Vector
 #  @param pattern (defaults to *) filters by vector name
 #
 proc SpecTcl_Vector/list {{pattern *}} {
-    set SpecTcl_Vector/lists application/json
+    set SpecTcl_Vector/list application/json
 
 
     set vectors [treeparamvec -list $pattern]
@@ -25,4 +25,114 @@ proc SpecTcl_Vector/list {{pattern *}} {
     }
     
     return [::SpecTcl::_returnObject "OK" [json::write array {*}$result]]
+}
+##
+# setlow
+#   Set the low value of a vector
+# @param name - name of the vector required.
+# @param low  - new low value
+# Result has an empty detail.
+#
+proc SpecTcl_Vector/setlow {{name ""} {low ""}}  {
+    set SpecTcl_Vector/setlow application/json
+
+    if {$name eq "" || $low  eq ""} {
+        return [::SpecTcl::_returnObject "missing parameter" \
+            [json::write string "need both 'name' and 'low' query parameters"]]
+    }
+    set status [catch {
+        treeparamvec -setlow $name $low
+    } msg ]
+
+    if {$status} {
+        # failed
+        return [::SpecTcl::_returnObject "bad status from treeparamvec command" \
+            [json::write string $msg]] 
+    }
+
+    return [::SpecTcl::_returnObject]
+}
+##
+# sethigh
+#   set the high limit value of a vector.
+#
+# @param name - name of the vector.
+# @param high - new high value
+# On success, result detail is empty.
+#
+proc SpecTcl_Vector/sethigh {{name ""} {high ""}} {
+    set SpecTcl_Vector/sethigh application/json
+
+    if {$name eq "" || $high  eq ""} {
+        return [::SpecTcl::_returnObject "missing parameter" \
+            [json::write string "need both 'name' and 'high' query parameters"]]
+    }
+
+    set status [catch {
+        treeparamvec -sethigh $name $high
+    } msg]
+
+    if {$status} {
+        # failed
+        return [::SpecTcl::_returnObject "bad status from treeparamvec command" \
+            [json::write string $msg]] 
+    }
+
+    return [::SpecTcl::_returnObject]
+}
+
+##
+# setbins
+#    set the binning of a vector
+#
+# @param name - vector name.
+# @param bins - number of bins desired.
+# on success the detauk is empty.
+#
+proc SpecTcl_Vector/setbins {{name ""} {bins ""}} {
+    set  SpecTcl_Vector/setbins application/json
+
+    if {$name eq "" || $bins  eq ""} {
+        return [::SpecTcl::_returnObject "missing parameter" \
+            [json::write string "need both 'name' and 'bins' query parameters"]]
+    }
+
+    set status [catch {
+        treeparamvec -setbins $name $bins
+    } msg]
+
+    if {$status} {
+        # failed
+        return [::SpecTcl::_returnObject "bad status from treeparamvec command" \
+            [json::write string $msg]] 
+    }
+
+    return [::SpecTcl::_returnObject]
+}
+
+## 
+# setunits
+#   Set a new units value for a vector
+#
+# @param name - name of the vector.
+# @param units - new units.
+# on success, the detail is nonexistent.
+#
+proc SpecTcl_Vector/setunits {{name ""} {units ""}} {
+    if {$name eq "" || $units  eq ""} {
+        return [::SpecTcl::_returnObject "missing parameter" \
+            [json::write string "need both 'name' and 'units' query parameters"]]
+    }
+
+    set status [catch {
+        treeparamvec -setunits $name $units
+    } msg]
+
+    if {$status} {
+        # failed
+        return [::SpecTcl::_returnObject "bad status from treeparamvec command" \
+            [json::write string $msg]] 
+    }
+
+    return [::SpecTcl::_returnObject]
 }
